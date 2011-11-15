9b6620ca7c730ab7556f91af6657ec72fefa4a1e
Miroslav Bendík <miroslav.bendik@gmail.com>
2011-11-15 21:48:38 +0100

    Oprava výberu používateľov

--- account/admin.py
+++ account/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
-from shakal.account.models import UserProfile
+from shakal.account.models import UserProfile, UserProxy
 
 class UserProfileInline(admin.StackedInline):
 	model = UserProfile
@@ -16,4 +16,5 @@ class UserAdmin(AuthUserAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(UserProxy, UserAdmin)
 
