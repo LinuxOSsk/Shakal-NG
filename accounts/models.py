4852b04b90f5bd4af3cd83c1cfec5c4273fbea76
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-07-21 13:50:09 +0200

    Pridané automatické vytvorenie profilu.

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.core.validators import MaxLengthValidator, MinValueValidator, MaxValueValidator
 from django.utils.translation import ugettext_lazy as _
@@ -11,4 +12,12 @@ class UserProfile(models.Model):
 	display_mail = models.BooleanField(default = False, verbose_name = _('display mail'))
 	distribution = models.CharField(max_length = 50, blank = True, verbose_name = _('linux distribution'))
 	info = models.TextField(validators = [MaxLengthValidator(100000)], blank = True, verbose_name = _('informations'))
-	year = models.SmallIntegerField(validators = [MinValueValidator(1900), MaxValueValidator(lambda: 2000)], blank = True, verbose_name = _('year of birth'))
+	year = models.SmallIntegerField(validators = [MinValueValidator(1900), MaxValueValidator(lambda: 2000)], blank = True, null = True, verbose_name = _('year of birth'))
+
+
+def create_user_profile(sender, **kwargs):
+	user = kwargs['instance']
+	if not UserProfile.objects.filter(user = user):
+		UserProfile(user = user).save()
+
+post_save.connect(create_user_profile, sender = User)
