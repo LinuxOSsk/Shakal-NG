136e7ca42b8b28aacc3b53d850f4e7a6b38761e2
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-07-24 11:30:32 +0200

    Pridaná podpora URL argumentov do breadcrumb.

--- breadcrumbs/templatetags/breadcrumbs_tags.py
+++ breadcrumbs/templatetags/breadcrumbs_tags.py
@@ -2,28 +2,31 @@
 
 from django.core.urlresolvers import reverse
 from django import template
-from shakal.utils import process_template_params
+from shakal.utils import process_template_args, process_template_kwargs
 
 register = template.Library()
 
 class BreadcrumbNode(template.Node):
-	def __init__(self, nodelist, bits):
+	def __init__(self, nodelist, required, optional):
 		self.nodelist = nodelist
-		self.bits = bits
+		self.required = required
+		self.optional = optional
 
 	def render(self, context):
 		contents = self.nodelist.render(context)
-		params = process_template_params(self.bits, context)
+		params = process_template_kwargs(self.required, context)
+		reverse_args = process_template_args(self.optional, context)
+		reverse_kwargs = process_template_kwargs(self.optional, context)
 
 		url = params.get('url', False)
 		if url:
-			url = reverse(url)
-		cls = params.get('class', False)
+			url = reverse(url, args = reverse_args, kwargs = reverse_kwargs)
+		class_name = params.get('class', False)
 
 		breadcrumb_context = {
 			'contents': contents,
 			'url': url,
-			'class': cls
+			'class': class_name
 		}
 
 		if not 'breadcrumbs' in context:
@@ -36,7 +39,14 @@ def breadcrumb(parser, token):
 	nodelist = parser.parse(('endbreadcrumb',))
 	parser.delete_first_token()
 	bits = token.split_contents()[1:]
-	return BreadcrumbNode(nodelist, bits)
+	optional_pos = 0
+	for bit in bits:
+		optional_pos += 1
+		if bit.find('url=') == 0:
+			break
+	required = bits[:optional_pos]
+	optional = bits[optional_pos:]
+	return BreadcrumbNode(nodelist, required, optional)
 
 
 @register.inclusion_tag('breadcrumbs/breadcrumbs.html', takes_context = True)
--- shakal/utils.py
+++ shakal/utils.py
@@ -2,19 +2,30 @@
 
 from django import template
 
-def process_template_params(rawparams, context = None):
-	params = {}
+
+def process_template_args(rawparams, context = None):
+	args = []
+	for param in rawparams:
+		pos = param.find('=')
+		if (pos > 0):
+			break
+		if context is not None:
+			param = template.resolve_variable(param, context)
+		args.append(param)
+	return args
+
+
+def process_template_kwargs(rawparams, context = None):
+	kwargs = {}
 	for param in rawparams:
 		paramname = param
 		paramvalue = ''
-		try:
-			pos = param.index('=')
-			paramname = param[:pos]
-			paramvalue = param[pos + 1:]
-			if context is not None:
-				paramvalue = template.resolve_variable(paramvalue, context)
-		except KeyError:
-			paramvalue = True
-			pass
-		params[paramname] = paramvalue
-	return params
+		pos = param.find('=')
+		if (pos <= 0):
+			continue
+		paramname = param[:pos]
+		paramvalue = param[pos + 1:]
+		if context is not None:
+			paramvalue = template.resolve_variable(paramvalue, context)
+		kwargs[paramname] = paramvalue
+	return kwargs
