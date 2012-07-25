59f46aaab4dfce9713279807a0e0fd5b3c49ad70
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-07-25 05:57:40 +0200

    Pridaný redirect na články z pôvodného LinuxOS.

--- shakal/linuxos/redirect_views.py
+++ shakal/linuxos/redirect_views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponsePermanentRedirect
+from shakal.article.models import Article
 
 def profile_redirect(request, pk):
 	return HttpResponsePermanentRedirect(reverse('auth_profile', kwargs = {'pk': pk}))
+
+def article_redirect(request, pk):
+	article = get_object_or_404(Article, pk = pk)
+	return HttpResponsePermanentRedirect(reverse('article:detail-by-slug', kwargs = {'slug': article.slug}))
