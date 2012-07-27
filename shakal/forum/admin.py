3f7c96f41f350fe78d81df6b39bc3577a2921acf
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-07-27 13:20:01 +0200

    Pridané témy fóra.

--- shakal/forum/admin.py
+++ shakal/forum/admin.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from shakal.forum.models import Section
+from shakal.forum.models import Section, Topic
 
 class SectionAdmin(admin.ModelAdmin):
 	list_display = ('name', 'slug', )
 	search_fields = ('name', 'slug', )
 	prepopulated_fields = {'slug': ('name', )}
 
+class TopicAdmin(admin.ModelAdmin):
+	list_display = ('title', 'get_username', )
+	search_fields = ('title', 'get_username', )
+	ordering = ('-id', )
+	raw_id_fields = ('user', )
+
 admin.site.register(Section, SectionAdmin)
+admin.site.register(Topic, TopicAdmin)
--- shakal/forum/models.py
+++ shakal/forum/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 class Section(models.Model):
@@ -14,3 +15,25 @@ class Section(models.Model):
 	class Meta:
 		verbose_name = _('section')
 		verbose_name_plural = _('sections')
+
+
+class Topic(models.Model):
+	title = models.CharField(max_length = 100, verbose_name = _('title'))
+	text = models.TextField()
+	time = models.DateTimeField()
+	username = models.CharField(max_length = 50, verbose_name = _('user name'))
+	user = models.ForeignKey(User, blank = True, null = True)
+
+	def get_username(self):
+		if self.user:
+			if self.user.get_full_name():
+				return self.user.get_full_name()
+			else:
+				return self.user.username
+		else:
+			return self.username
+	get_username.short_description = _('user name')
+
+	class Meta:
+		verbose_name = _('topic')
+		verbose_name_plural = _('topics')
