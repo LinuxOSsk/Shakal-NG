944db51b9d0161b22de1720adb3b2ac6f0e27e6d
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-07-28 18:18:21 +0200

    Pridaný náhľad pri odosielaní témy.

--- shakal/forum/views.py
+++ shakal/forum/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from django.views.generic.edit import ModelFormMixin
 from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 from django.template import RequestContext
 from django.views.generic import CreateView
+from datetime import datetime
 from forms import TopicForm, TopicLoggedForm
 from models import Section, Topic
 
@@ -43,10 +43,13 @@ class TopicCreateView(CreateView):
 
 	def form_valid(self, form):
 		topic = form.save(commit = False)
+		topic.time = datetime.now()
 		if self.request.user.is_authenticated():
 			if self.request.user.get_full_name():
 				topic.username = self.request.user.get_full_name()
 			else:
 				topic.username = self.request.user.username
 			topic.user = self.request.user
+		if not 'create' in self.request.POST:
+			return self.render_to_response(self.get_context_data(form = form, topic = topic, valid = True))
 		return super(TopicCreateView, self).form_valid(form)
