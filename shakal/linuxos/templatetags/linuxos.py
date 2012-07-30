fc735a6a066465e2fd337f4855abe70ab5db3c3f
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-07-30 09:23:09 +0200

    Prepracované zobrazovanie užívateľských správ.

--- shakal/linuxos/templatetags/linuxos.py
+++ shakal/linuxos/templatetags/linuxos.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django import template
+from django.template.loader import render_to_string
 from django.utils.html import escape
 from datetime import date, timedelta
 
 register = template.Library()
+
 
 @register.filter
 def humandatetime(value):
@@ -19,6 +21,7 @@ def humandatetime(value):
 		else:
 			return value.strftime("%d.%m | %H:%M")
 
+
 @register.simple_tag
 def user_link(user_object, username):
 	if user_object:
@@ -31,3 +34,48 @@ def user_link(user_object, username):
 		return tpl.render(ctx)
 	else:
 		return escape(username)
+
+
+class MessagesNode(template.Node):
+	def __init__(self, messages, tags):
+		self.messages = messages
+		self.tags = tags
+
+	def render(self, context):
+		messages = template.resolve_variable(self.messages, context)
+		tags = [template.resolve_variable(tag, context) for tag in self.tags]
+		if not tags:
+			messages = self.__filter_exact_tags(messages, ['debug', 'info', 'success', 'warning', 'error'])
+		else:
+			messages = self.__filter_contains_tags(messages, tags)
+		search_templates = []
+		if tags:
+			search_templates.append('messages/messages_' + tags.implode('_') + '.html')
+		search_templates.append('messages/messages.html')
+		return render_to_string(search_templates, {'messages': messages})
+
+	def __filter_exact_tags(self, messages, tags):
+		new_messages = []
+		for message in messages:
+			if message.tags in tags:
+				new_messages.append(message)
+		return new_messages
+
+	def __filter_contains_tags(self, messages, tags):
+		new_messages = []
+		for message in messages:
+			if message.tags:
+				message_tags = message.tags.split(' ')
+				for message_tag in message_tags:
+					if message_tag in tags:
+						new_messages.append(message)
+						break
+		return new_messages
+
+
+@register.tag
+def render_messages(parser, token):
+	parts = token.split_contents()
+	if len(parts) < 2:
+		raise template.TemplateSyntaxError('{0} tags requires messages variable.'.format(token.contents.split()[0]))
+	return MessagesNode(parts[1], parts[2:])
