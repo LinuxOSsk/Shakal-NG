45cdf12cc6ef8737c472fff27d01aecc95e81fd7
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-07-31 18:09:26 +0200

    Pridaný formulár na vytvorenie komentára.

--- shakal/threaded_comments/forms.py
+++ shakal/threaded_comments/forms.py
@@ -3,12 +3,15 @@
 from django import forms
 from django.contrib.comments.forms import CommentForm, COMMENT_MAX_LENGTH
 from django.utils.translation import ugettext_lazy as _
+from time import time
+from models import ThreadedComment
+
 
 class ThreadedCommentForm(CommentForm):
-	subject = forms.CharField(max_length = 100),
-	url = forms.URLField(widget = forms.HiddenInput, required = False),
-	parent_pk = forms.IntegerField(widget = forms.HiddenInput, required = False),
-	comment = forms.CharField(label = _("Comment"), max_length = COMMENT_MAX_LENGTH)
+	subject = forms.CharField(label = _("Subject"), max_length = COMMENT_MAX_LENGTH)
+	url = forms.URLField(label = _("URL"), widget = forms.HiddenInput, required = False)
+	parent_pk = forms.IntegerField(label = _("Parent PK"), widget = forms.HiddenInput, required = False)
+	comment = forms.CharField(label = _("Comment"), max_length = COMMENT_MAX_LENGTH, widget = forms.Textarea)
 
 	def __init__(self, *args, **kwargs):
 		self.__parent_comment = kwargs.pop('parent_comment', None)
@@ -25,3 +28,44 @@ class ThreadedCommentForm(CommentForm):
 			'security_hash',
 			'parent_pk'
 		]
+
+	def get_comment_model(self):
+		return ThreadedComment
+
+	def get_comment_object(self):
+		comment = super(ThreadedCommentForm, self).get_comment_object()
+		parent_pk = self.cleaned_data.get('parent_pk')
+		parent_comment = ThreadedComment.objects.get(pk = parent_pk)
+		comment.subject  = self.cleaned_data['subject']
+		comment.parent = parent_comment
+		return comment
+
+	def get_comment_dict(self):
+		return {
+			'subject' : self.data.get('subject'),
+			'name'    : self.data.get('name'),
+			'email'   : self.data.get('email'),
+			'url'     : self.data.get('url'),
+			'comment' : self.data.get('comment'),
+		}
+
+	def generate_security_data(self):
+		timestamp = int(time())
+		security_dict =   {
+			'content_type'  : str(self.target_object._meta),
+			'object_pk'     : str(self.target_object._get_pk_val()),
+			'parent_pk'     : str(self.__parent_comment.pk),
+			'timestamp'     : str(timestamp),
+			'security_hash' : self.initial_security_hash(timestamp),
+		}
+		return security_dict
+
+	def get_comment_create_data(self):
+		data = super(ThreadedCommentForm, self).get_comment_create_data()
+		data['subject'] = self.cleaned_data['subject']
+		data['url'] = ''
+		return data
+
+	def clean_comment(self):
+		data = self.cleaned_data['comment']
+		return data
--- shakal/threaded_comments/views.py
+++ shakal/threaded_comments/views.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 
-def reply_comment(request):
-	pass
+from django.template.response import TemplateResponse
+from shakal.threaded_comments.models import ThreadedComment
+from shakal.threaded_comments import get_form
+
+def reply_comment(request, parent):
+	parent_comment = ThreadedComment.objects.get(pk = parent)
+	content_object = parent_comment.content_object
+	form = get_form()(content_object, parent_comment = parent_comment)
+
+	model_meta = content_object.__class__._meta
+	template_list = [
+		"comments/{0}_{1}_new_form.html".format(*tuple(str(model_meta).split('.'))),
+		"comments/{0}_new_form.html".format(model_meta.app_label),
+		"comments/new_form.html",
+	]
+	return TemplateResponse(request, template_list, {'form': form, 'next': content_object.get_absolute_url() })
 
 def post_comment(request):
 	pass
