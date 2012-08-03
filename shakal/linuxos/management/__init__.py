6a61bbebbce75c50c42ae0fb567a4270ea35f669
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-08-03 20:47:57 +0200

    Predpripravené stĺpce a tabuľky pre view.

--- shakal/linuxos/management/__init__.py
+++ shakal/linuxos/management/__init__.py
@@ -26,15 +26,17 @@ def install_view(connection, source_table, join_tables, extra_columns, content_t
 
 def install_views(sender, **kwargs):
 	connection = connections[kwargs['db']]
+	join_tables = ['threaded_comments_rootheader', 'hitcount_hitcount']
+	extra_columns = [
+		{'comment_count': 'comment_count', 'last_comment': 'last_comment'},
+		{'display_count': 'hits'}
+	]
 	if connection.vendor == 'postgresql':
 		install_view(
 			connection,
 			'article_article',
-			['threaded_comments_rootheader', 'hitcount_hitcount'],
-			[
-				{'comment_count': 'comment_count', 'last_comment': 'last_comment'},
-				{'display_count': 'hits'}
-			],
+			join_tables,
+			extra_columns,
 			ContentType.objects.get(app_label = 'article', model = 'articleview').pk
 		)
 
