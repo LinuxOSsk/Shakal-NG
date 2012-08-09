4e18ff7085869e295d8358cf722ee988807d4b7a
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-08-09 15:48:32 +0200

    Opraven zobrazovanie počtu komentárov pri anketách.

--- shakal/survey/models.py
+++ shakal/survey/models.py
@@ -4,12 +4,10 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
-from django.db import connection, models
+from django.db import models
 from django.db.models import permalink
-from django.db.models.query import QuerySet
 from django.utils.translation import ugettext_lazy as _
-from generic_aggregation import generic_annotate
-from shakal.threaded_comments.models import RootHeader
+from shakal.threaded_comments.models import CommentCountManager, RootHeader
 
 class SurveyAbstract(models.Model):
 	question = models.TextField(verbose_name = _("question"))
@@ -53,21 +51,15 @@ class SurveyAbstract(models.Model):
 		abstract = True
 
 
-class SurveyListManager(models.Manager):
+class SurveyListManager(CommentCountManager):
 	def get_query_set(self):
-		if connection.vendor == 'postgresql':
-			queryset = QuerySet(SurveyView, using = self._db)
-			queryset = queryset.extra(select = {'last_comment': 'last_comment', 'comment_count': 'comment_count'})
-		else:
-			queryset = QuerySet(Survey, using = self._db)
-			queryset = generic_annotate(queryset, RootHeader, models.Max('comments_header__last_comment'), alias = 'last_comment')
-			queryset = generic_annotate(queryset, RootHeader, models.Max('comments_header__comment_count'), alias = 'comment_count')
-		return queryset
+		return super(SurveyListManager, self).get_query_set(SurveyView, Survey)
 
 
 class Survey(SurveyAbstract):
 	objects = models.Manager()
 	surveys = SurveyListManager()
+	comments_header = generic.GenericRelation(RootHeader)
 
 	class Meta:
 		verbose_name = _('survey')
