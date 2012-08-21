993f3c178d323c21dde0fd9bbcc287d4fa13a8cc
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-08-21 18:17:51 +0200

    Pridaná veľkosť prílohy.

--- attachment/models.py
+++ attachment/models.py
@@ -8,15 +8,17 @@ import os
 
 class Attachment(models.Model):
 	def upload_dir(instance, filename):
-		return 'attachment/{0}_{1}/{2}/{3}'.format(
+		return 'attachment/{0}_{1}/{2}/{3:02x}/{4}'.format(
 			instance.content_object._meta.app_label,
 			instance.content_object._meta.object_name.lower(),
+			instance.content_object.pk,
 			instance.content_object.pk,
 			filename
 		)
 
 	attachment = models.FileField(verbose_name = _('attachment'), upload_to = upload_dir)
 	created = models.DateTimeField(auto_now_add = True, verbose_name = _('created'))
+	size = models.IntegerField(verbose_name = _('size'))
 	content_type = models.ForeignKey(ContentType)
 	object_id = models.PositiveIntegerField()
 	content_object = generic.GenericForeignKey('content_type', 'object_id')
@@ -24,6 +26,17 @@ class Attachment(models.Model):
 	def delete(self, *args, **kwargs):
 		super(Attachment, self).delete(*args, **kwargs)
 		self.attachment.storage.delete(self.attachment.path)
+
+	def save(self, *args, **kwargs):
+		if self.pk:
+			try:
+				original = Attachment.objects.get(pk = self.pk)
+				if self.attachment != original.attachment:
+					original.attachment.storage.delete(original.attachment.path)
+			except:
+				pass
+		self.size = self.attachment.size
+		super(Attachment, self).save(*args, **kwargs)
 
 	@property
 	def name(self):
