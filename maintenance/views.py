8422275b795c83a6b32178759aebe15fb163cdd4
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-10-24 20:52:55 +0200

    Implementovaná automatická aktualizácia stavu.

--- maintenance/views.py
+++ maintenance/views.py
@@ -7,6 +7,8 @@ import socket
 def status(request):
 	status = "{}"
 	address = os.path.abspath("maintenance.flag")
+	if (os.path.exists(address)):
+		status = "{text: '...'}"
 	sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 	try:
 		sock.connect(address)
