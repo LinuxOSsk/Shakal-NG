0443d525fc89213c9f74266eab85a6dce7121cca
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-11-26 22:40:23 +0100

    Zmena css za less.

---
+++ shakal/linuxos/management/commands/compress_less.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+from subprocess import call
+import os
+
+class Command(BaseCommand):
+	args = ''
+	help = 'Cron'
+
+	def __init__(self, *args, **kwargs):
+		super(Command, self).__init__(*args, **kwargs)
+
+	def handle(self, *args, **kwargs):
+		root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", ".."))
+		input_path = os.path.join(root_path, 'static', 'desktop', 'default', 'base.less')
+		output_path = os.path.join(root_path, 'static', 'desktop', 'default', 'base.css')
+		call(['lessc', input_path, '-x', '--yui-compress', '--02'], stdout = open(output_path, 'w'))
