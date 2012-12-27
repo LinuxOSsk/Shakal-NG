8fe3508795bd215f2c6f3a2aee15801382376ebb
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-12-27 18:40:06 +0100

    Pridané dekódovanie XML entít.

--- shakal/search/templatetags/html_entity_decode.py
+++ shakal/search/templatetags/html_entity_decode.py
@@ -7,6 +7,7 @@ from django import template
 register = template.Library()
 
 pattern = re.compile("&(\w+?);")
+dec_pattern = re.compile("&\\#(\d+?);")
 
 def html_entity_decode_char(m, defs=htmlentitydefs.entitydefs):
 	try:
@@ -14,6 +15,12 @@ def html_entity_decode_char(m, defs=htmlentitydefs.entitydefs):
 	except KeyError:
 		return m.group(0)
 
+def xml_entity_decode_char(m):
+	try:
+		return unichr(int(m.group(1)))
+	except:
+		return m
+
 @register.filter
 def html_entity_decode(string):
-	return pattern.sub(html_entity_decode_char, string)
+	return dec_pattern.sub(xml_entity_decode_char, pattern.sub(html_entity_decode_char, string))
