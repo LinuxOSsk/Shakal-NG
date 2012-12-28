e254c4b17fa900b9d169034a590a510954ace848
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-12-28 13:55:11 +0100

    Implementovaný inštalačný skript.

--- README.rst
+++ README.rst
@@ -8,16 +8,6 @@ Autor:
 Inštalácia
 ----------
 
-Pre pohodlnejšiu prácu odporúčam nainštalovať virtualenvwrapper. Následne
-spustiť source `source cest/k/virtualenvwrapper.sh`. Pre vytvorenie nového
-prostredia pre shakal sa použije príkaz `mkvirtualenv shakal` a prostredie sa
-aktivuje príkazom `workon django`.
-
-Závislosti
-^^^^^^^^^^
-
-Závislosti sa dajú automaticky nainštalovať nástrojom `pip`:
-
 ::
 
-    pip install -r requirements.txt
+    wget https://raw.github.com/mireq/Shakal-NG/master/install.sh&&chmod +x install.sh&&. ./install.sh
---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+PYTHON=python2.7
+
+mkdir shakal
+git clone https://github.com/mireq/Shakal-NG.git shakal/shakal-src
+mv -f shakal/shakal-src/* shakal
+mv -f shakal/shakal-src/.git shakal
+mv -f shakal/shakal-src/.gitignore shakal
+rmdir shakal/shakal-src
+wget https://raw.github.com/pypa/virtualenv/master/virtualenv.py -O shakal/virtualenv.py
+$PYTHON shakal/virtualenv.py --distribute shakal/venv -p $PYTHON
+. shakal/venv/bin/activate
+pip install -r shakal/requirements.txt
+git clone git://github.com/cloudhead/less.js.git shakal/venv/less
+cd shakal
+cd venv
+npm -l i less
+cd bin
+ln -s ../node_modules/less/bin/lessc .
+cd ../..
+python manage.py syncdb
+python manage.py compress_images
+python manage.py compress_less
