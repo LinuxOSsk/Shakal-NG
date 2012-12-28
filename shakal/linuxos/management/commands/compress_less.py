6cc50b6fa2bdb5fda72f34950cc539bdedf2e771
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-12-28 13:45:25 +0100

    Opravený kompresný skript.

--- shakal/linuxos/management/commands/compress_less.py
+++ shakal/linuxos/management/commands/compress_less.py
@@ -19,4 +19,4 @@ class Command(BaseCommand):
 		root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", ".."))
 		input_path = os.path.join(root_path, 'static', 'desktop', 'default', 'base.less')
 		output_path = os.path.join(root_path, 'static', 'desktop', 'default', 'base.css')
-		call(['lessc', input_path, '-x', '--yui-compress', '--02'], stdout = open(output_path, 'w'), stderr = sys.stderr)
+		call(['lessc', input_path, '-x', '--yui-compress', '-O2'], stdout = open(output_path, 'w'), stderr = sys.stderr)
