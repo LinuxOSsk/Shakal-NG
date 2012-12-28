fbe6f49dc7793ba89257025245c3246d752e2c90
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-12-28 14:11:35 +0100

    Added initial checks.

--- install.sh
+++ install.sh
@@ -2,6 +2,27 @@
 
 PYTHON=python2.7
 
+if which npm > /dev/null
+	then echo "Node.js .. OK"
+else
+	echo "No Node.js"
+	exit
+fi
+
+if which git > /dev/null
+	then echo "git .. OK"
+else
+	echo "No git"
+	exit
+fi
+
+if which $PYTHON > /dev/null
+	then echo "${PYTHON} .. OK"
+else
+	echo "No ${PYTHON}"
+	exit
+fi
+
 mkdir shakal
 git clone https://github.com/mireq/Shakal-NG.git shakal/shakal-src
 mv -f shakal/shakal-src/* shakal
