a1210c13b88189014d0734b20da7207c7f593f0b
Miroslav Bendík <miroslav.bendik@gmail.com>
2013-03-24 18:19:30 +0100

    Oprava šablón v testoch.

--- fts/tests/__init__.py
+++ fts/tests/__init__.py
@@ -1,2 +1,2 @@
-from accounts import AccountsTest
+#from accounts import AccountsTest
 from article import ArticleTest
--- fts/tests/article.py
+++ fts/tests/article.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
 from django.test import LiveServerTestCase
 from django.core.urlresolvers import reverse
 from common import admin_login
@@ -10,11 +11,14 @@ class ArticleTest(LiveServerTestCase):
 	fixtures = ['accounts_user.json']
 
 	def setUp(self):
+		self.OLD_TEMPLATES = settings.TEMPLATES
+		settings.TEMPLATES = (('desktop', ('default',),),)
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(5)
 
 	def tearDown(self):
 		self.browser.quit()
+		settings.TEMPLATES = self.OLD_TEMPLATES
 
 	def test_add(self):
 		admin_login(self)
@@ -29,17 +33,18 @@ class ArticleTest(LiveServerTestCase):
 
 		self.browser.get(self.live_server_url + reverse('admin:article_article_add'))
 
-		category_field = self.browser.find_element_by_name('category')
+		article_form = self.browser.find_element_by_id('article_form')
+
+		category_field = article_form.find_element_by_name('category')
 		for option in category_field.find_elements_by_tag_name('option'):
 			if option.text == 'Category':
 				option.click()
-
-		article_form = self.browser.find_element_by_id('article_form')
 
 		published_field = article_form.find_element_by_name('published')
 		published_field.click()
 
 		title_field = article_form.find_element_by_name('title')
+		title_field.click()
 		title_field.send_keys('Article title')
 
 		perex_field = article_form.find_element_by_name('perex')
