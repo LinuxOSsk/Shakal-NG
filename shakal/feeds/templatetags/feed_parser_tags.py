70d8e75c5b6a1fba686e76cd3e78d66f94d3e3e7
Miroslav Bendík <miroslav.bendik@gmail.com>
2013-03-31 21:01:51 +0200

    Úprava na nové API feedparser.

--- shakal/feeds/templatetags/feed_parser_tags.py
+++ shakal/feeds/templatetags/feed_parser_tags.py
@@ -6,17 +6,21 @@ import feedparser
 
 register = template.Library()
 
+
 @register.inclusion_tag("feeds/pull_feeds.html")
 def pull_feeds(url, max_count = 4):
 	posts = []
 	feed = feedparser.parse(url)
 	for i in range(max_count):
-		pub_date = feed['entries'][i].updated_parsed
+		try:
+			pub_date = feed.entries[i].published_parsed
+		except AttributeError:
+			pub_date = feed.entries[i].updated_parsed
 		published = datetime.date(pub_date[0], pub_date[1], pub_date[2])
 		try:
 			posts.append({
-				'title': feed['entries'][i].title,
-				'link': feed['entries'][i].link,
+				'title': feed.entries[i].title,
+				'link': feed.entries[i].link,
 				'published': published,
 			})
 		except IndexError:
