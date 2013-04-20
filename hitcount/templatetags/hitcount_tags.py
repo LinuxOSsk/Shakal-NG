4a995ce463649735ce7aafeaebc6d177f6a6a663
Miroslav Bendík <miroslav.bendik@gmail.com>
2013-04-20 18:52:09 +0200

    Zjednodušenie kódu počtu návštev.

--- hitcount/templatetags/hitcount_tags.py
+++ hitcount/templatetags/hitcount_tags.py
@@ -13,15 +13,15 @@ register = template.Library()
 def add_hitcount(context, model):
 	model = iterify(model)
 	content_type = None
-	id_list = []
-	for obj in model:
-		id_list.append(obj.pk)
-		if content_type is None:
-			content_type = ContentType.objects.get_for_model(type(obj))
-	if not content_type:
+	if len(model) == 0:
 		return ''
-	hitcounts = HitCount.objects.filter(content_type = content_type, object_id__in = id_list).values('object_id', 'hits')
-	hitcounts = dict([(h['object_id'], h['hits']) for h in hitcounts])
+	id_list = [o.pk for o in model]
+	content_type = ContentType.objects.get_for_model(model[0].__class__)
+	hitcounts = HitCount \
+		.objects \
+		.filter(content_type = content_type, object_id__in = id_list) \
+		.values_list('object_id', 'hits')
+	hitcounts = dict(hitcounts)
 	for obj in model:
 		obj.display_count = hitcounts.get(obj.pk, 0)
 	return ''
