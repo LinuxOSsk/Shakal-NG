b5eaf62839a0be1dff332e699820e58e3f56d29a
Miroslav Bendík <miroslav.bendik@gmail.com>
2013-05-01 13:57:56 +0200

    Prečistený výber formátu.

--- rich_editor/widgets.py
+++ rich_editor/widgets.py
@@ -23,20 +23,8 @@ class RichEditor(Textarea):
 		attrs.update({'class': 'wymeditor'})
 		super(RichEditor, self).__init__(attrs)
 
-	def render(self, name, value, attrs = None, **kwargs):
-		supported_tags = self.attrs.pop('supported_tags', {})
-		format_inputs = []
-		if value is None:
-			value_fmt = None
-			value = None
-		else:
-			value_fmt, value = value
-		for fmt, label in self.formats:
-			checked = ' checked="checked"' if fmt == value_fmt else ''
-			input_text = '<input type="radio" name="' + mark_safe(name + '_format') + '" value="' + mark_safe(fmt) + '" ' + checked + '/> '
-			format_inputs.append('<label>' + input_text + mark_safe(label) + '</label>')
-		widget = mark_safe(u'<div class="textareawrapper">' + ''.join(format_inputs)) + super(RichEditor, self).render(name, value, attrs) + mark_safe(u'</div>')
-		self.attrs['supported_tags'] = supported_tags
+	def get_tags_info(self):
+		supported_tags = self.attrs.get('supported_tags', {})
 		unsupported_tags = set(ALL_TAGS) - set(supported_tags.keys()) - set(('html', 'body'))
 		defined_tags = set(supported_tags.keys()) - set('')
 		for tag in supported_tags:
@@ -45,16 +33,35 @@ class RichEditor(Textarea):
 			else:
 				tag_name = tag
 			unsupported_tags.update([tag_name + ' > ' + t for t in defined_tags - supported_tags[tag].get_child_tags() if t != ''])
-		tags_info = {
+		return {
 			'unsupported': list(unsupported_tags),
 			'known': supported_tags.keys(),
 		}
+
+	def render(self, name, value, attrs = None, **kwargs):
+		if value is None:
+			value = (None, None)
+		value_fmt, value = value
+
+		formats = []
+		for fmt, label in self.formats:
+			formats.append({'format': fmt, 'label': label, 'checked': fmt == value_fmt})
+		if not value_fmt in dict(self.formats):
+			formats[0]['checked'] = True
+
+		supported_tags = self.attrs.pop('supported_tags', {})
+		widget = super(RichEditor, self).render(name, value, attrs)
+		self.attrs['supported_tags'] = supported_tags
+
 		context = {
 			'name': name,
 			'lang': self.language[:2],
-			'tags': dumps(tags_info),
+			'tags': dumps(self.get_tags_info()),
+			'widget': widget,
+			'format': value_fmt,
+			'formats': formats
 		}
-		return widget + mark_safe(render_to_string('widgets/editor.html', context))
+		return mark_safe(render_to_string('widgets/editor.html', context))
 
 	def value_from_datadict(self, data, files, name):
 		text = data.get(name)
