3c887c9e152884894b2379e834e5bf9175f47363
Miroslav Bendík <miroslav.bendik@gmail.com>
2013-05-05 14:59:54 +0200

    Oprava ciest.

--- linuxos/management/commands/compress_less.py
+++ linuxos/management/commands/compress_less.py
@@ -16,7 +16,7 @@ class Command(BaseCommand):
 	def handle(self, *args, **kwargs):
 		c = Compressor()
 		c.savePositions()
-		root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", ".."))
+		root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
 		input_path = os.path.join(root_path, 'static', 'desktop', 'default', 'base.less')
 		output_path = os.path.join(root_path, 'static', 'desktop', 'default', 'base.css')
 		call(['lessc', input_path, '-x', '--yui-compress', '-O2'], stdout = open(output_path, 'w'), stderr = sys.stderr)
