22ddd97611769bedf39e13269aba8fcb71dcccb1
Miroslav Bendík <miroslav.bendik@gmail.com>
2013-05-11 14:26:38 +0200

    Optimalizácia vytvorenia definicíí tagov.

--- rich_editor/__init__.py
+++ rich_editor/__init__.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 from rich_editor.parser import HtmlParser, HtmlTag, HrefValidator, NofollowValidator, ONELINE_TAGS, DEFAULT_TAGS
-from copy import deepcopy
+from copy import deepcopy, copy
 
 
 def get_parser(editor_type):
 	if editor_type == 'signature':
-		supported_tags = deepcopy(ONELINE_TAGS)
+		supported_tags = copy(ONELINE_TAGS)
+		supported_tags['a'] = deepcopy(supported_tags['a'])
 		supported_tags['a'].req_attributes['rel'] = 'nofollow'
 		supported_tags['a'].attribute_validators = {'rel': [NofollowValidator()]}
 		return HtmlParser(supported_tags = supported_tags)
 	elif editor_type == 'profile':
-		supported_tags = deepcopy(DEFAULT_TAGS)
+		supported_tags = copy(DEFAULT_TAGS)
 		supported_tags['img'] = HtmlTag('img', opt = ['title'], req_attributes = {'src': '', 'alt': ''}, empty = True, attribute_validators = {'src': [HrefValidator()]})
-		supported_tags[''] = HtmlTag('', opt = ['', 'a', 'b', 'u', 'br', 'p', 'i', 'em', 'code', 'strong', 'pre', 'blockquote', 'ol', 'ul', 'span', 'cite', 'img'])
+		supported_tags[''] = deepcopy(supported_tags[''])
+		supported_tags[''].opt.add('img')
 		return HtmlParser(supported_tags = supported_tags)
 	else:
 		return HtmlParser()
