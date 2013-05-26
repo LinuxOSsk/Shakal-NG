1eb0ea7914832592237d0cd11219c192a437063a
Miroslav Bendík <miroslav.bendik@gmail.com>
2013-05-26 20:58:00 +0200

    Úprava zoraďovania užívateľov.

--- accounts/admin.py
+++ accounts/admin.py
@@ -9,6 +9,7 @@ from accounts.models import User
 class UserAdmin(AuthUserAdmin):
 	form = LessRestrictiveUserChangeForm
 	add_form = LessRestrictiveUserCreationForm
+	ordering = ('-id', )
 
 
 admin.site.register(User, UserAdmin)
