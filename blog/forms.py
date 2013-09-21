6ed57f92a07e07c09fd15d15abd1488abccf4f23
Miroslav Bendík <miroslav.bendik@gmail.com>
2013-09-21 17:48:18 +0200

    Pridaná kontrola času publikácie a možnosť publikoať okamžite.

--- blog/forms.py
+++ blog/forms.py
@@ -1,26 +1,35 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from django.utils.timezone import now
 
 from .models import Blog, Post
 from rich_editor.forms import RichOriginalField
 
 
 class BlogForm(forms.ModelForm):
-	original_description = RichOriginalField(Blog._meta.get_field('original_description').parsers, label = u'Popis', max_length = 1000, js = True) #pylint: disable=W0212
-	original_sidebar = RichOriginalField(Blog._meta.get_field('original_sidebar').parsers, label = u'Bočný panel', max_length = 1000, js = True) #pylint: disable=W0212
+	original_description = RichOriginalField(Blog._meta.get_field('original_description').parsers, label=u'Popis', max_length=1000, js=True) #pylint: disable=W0212
+	original_sidebar = RichOriginalField(Blog._meta.get_field('original_sidebar').parsers, label=u'Bočný panel', max_length=1000, js=True) #pylint: disable=W0212
 	class Meta:
 		model = Blog
 		exclude = ('author', 'slug')
 
 
 class PostForm(forms.ModelForm):
-	original_perex = RichOriginalField(Post._meta.get_field('original_perex').parsers, label = u'Perex', max_length = 1000, js = True) #pylint: disable=W0212
-	original_content = RichOriginalField(Post._meta.get_field('original_content').parsers, label = u'Obsah', max_length = 100000, js = True) #pylint: disable=W0212
+	original_perex = RichOriginalField(Post._meta.get_field('original_perex').parsers, label=u'Perex', max_length=1000, js=True) #pylint: disable=W0212
+	original_content = RichOriginalField(Post._meta.get_field('original_content').parsers, label=u'Obsah', max_length=100000, js=True) #pylint: disable=W0212
+	pub_now = forms.BooleanField(label=u'Publikovať teraz', required=False)
 
 	def __init__(self, *args, **kwargs):
 		super(PostForm, self).__init__(*args, **kwargs)
-		if self.instance and self.instance.published(): #pylint: disable=E1101
+		if self.instance and self.instance.published():
 			del self.fields['pub_time']
+
+	def clean_pub_time(self):
+		if 'pub_now' in self.data and self.data['pub_now']:
+			return now()
+		if self.cleaned_data['pub_time'] < now():
+			raise forms.ValidationError("Čas publikácie nesmie byť v minulosti")
+		return self.cleaned_data['pub_time']
 
 	class Meta:
 		model = Post
