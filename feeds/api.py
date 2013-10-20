eab5864c487339d22f8233ccb52656a287f27a67
Miroslav Bendík <miroslav.bendik@gmail.com>
2013-10-20 14:50:29 +0200

    Pridaný chýbajúci odkaz na blog.

--- feeds/api.py
+++ feeds/api.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 __all__ = ('register_feed', )
+from django.contrib.syndication.views import Feed
 
 
 def register_feed(request, feed, object_type = None, object_id = None):
 	if hasattr(request, '_feeds'):
 		feed_data = {
-			'title': feed.title,
-			'url': feed.feed_url,
+			'title': feed.title() if callable(feed.title) else feed.title,
+			'url': feed.feed_url() if callable(feed.feed_url) else feed.feed_url,
 			'object_type': object_type,
 			'object_id': object_id,
 		}
--- feeds/middleware.py
+++ feeds/middleware.py
@@ -10,8 +10,8 @@ from threaded_comments.feeds import CommentFeed
 class FeedsMiddleware(object):
 	def process_request(self, request):
 		setattr(request, '_feeds', [])
-		register_feed(request, ArticleFeed)
-		register_feed(request, TopicFeed)
-		register_feed(request, NewsFeed)
-		register_feed(request, PostFeed)
-		register_feed(request, CommentFeed)
+		register_feed(request, ArticleFeed())
+		register_feed(request, TopicFeed())
+		register_feed(request, NewsFeed())
+		register_feed(request, PostFeed())
+		register_feed(request, CommentFeed())
