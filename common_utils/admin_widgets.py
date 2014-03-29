1b3520fede8b4539e254b4db4c5522d709122ac0
Miroslav Bendík <miroslav.bendik@gmail.com>
2014-03-29 14:00:36 +0100

    Potlačené varovania pylint.

--- common_utils/admin_widgets.py
+++ common_utils/admin_widgets.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#pylint: disable=W0611,F0401
 try:
 	from suit.widgets import SuitDateWidget as DateInput, SuitTimeWidget as TimeInput, SuitSplitDateTimeWidget as DateTimeInput, EnclosedInput, AutosizedTextarea
 except ImportError:
