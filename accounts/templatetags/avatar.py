28f5bd9b78f940ea39b7edd8691905078b8ee8fd
Miroslav Bendík <miroslav.bendik@gmail.com>
2014-04-21 16:13:39 +0200

    Zrýchlené generovanie gravataru.

--- accounts/templatetags/avatar.py
+++ accounts/templatetags/avatar.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import hashlib
-import urllib
 from django import template
 from django.conf import settings
 from django.utils.html import escape
@@ -19,8 +18,7 @@ GRAVATAR_DEFAULT_SIZE = getattr(settings, "GRAVATAR_DEFAULT_IMAGE", 200)
 @lib.global_function
 @register.simple_tag
 def gravatar_for_email(email, size=GRAVATAR_DEFAULT_SIZE):
-	url = "%savatar/%s/?" % (GRAVATAR_URL_PREFIX, hashlib.md5(bytes(email.encode('utf-8'))).hexdigest())
-	url += urllib.urlencode({"s": str(size), "default": GRAVATAR_DEFAULT_IMAGE})
+	url = "%savatar/%s/?s=%s&default=%s" % (GRAVATAR_URL_PREFIX, hashlib.md5(bytes(email.encode('utf-8'))).hexdigest(), str(size), GRAVATAR_DEFAULT_IMAGE)
 	return escape(url)
 
 
