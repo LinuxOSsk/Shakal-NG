5a0822ebde4511f14383687e88559b2f5345751f
Miroslav Bendík <miroslav.bendik@gmail.com>
2014-04-24 20:37:13 +0200

    Oprava CSRF middleware tokenu vo ankete.

--- polls/templatetags/polls_tags.py
+++ polls/templatetags/polls_tags.py
@@ -3,6 +3,7 @@ from django import template
 from django.template.loader import render_to_string
 from django_jinja import library
 from jinja2 import contextfunction
+from jinja2 import nodes
 
 from polls.models import Poll
 
@@ -13,11 +14,12 @@ lib = library.Library()
 
 @lib.global_function
 @contextfunction
-@register.simple_tag(takes_context = True)
+@register.simple_tag(takes_context=True)
 def polls_frontpage(context):
-	ctx = {
+	ctx = context.get_all()
+	ctx.update({
 		'polls': Poll.objects.all()[:1],
 		'request': context['request'],
 		'user': context['user'],
-	}
+	})
 	return render_to_string('polls/block_poll_detail.html', ctx)
