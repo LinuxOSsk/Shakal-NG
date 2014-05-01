707f572699a07022327b19723b158a596df585ca
Miroslav Bendík <miroslav.bendik@gmail.com>
2014-05-01 23:10:38 +0200

    Zmenené polls_frontpage tak, aby bolo kompatibilné s jinja2.

--- polls/templatetags/polls_tags.py
+++ polls/templatetags/polls_tags.py
@@ -3,7 +3,6 @@ from django import template
 from django.template.loader import render_to_string
 from django_jinja import library
 from jinja2 import contextfunction
-from jinja2 import nodes
 
 from polls.models import Poll
 
@@ -16,19 +15,9 @@ lib = library.Library()
 @contextfunction
 @register.simple_tag(takes_context=True)
 def polls_frontpage(context):
-	ctx = context.get_all()
-	ctx.update({
-		'polls': Poll.objects.all()[:1],
-		'request': context['request'],
-		'user': context['user'],
-	})
-	return render_to_string('polls/block_poll_detail.html', ctx)
-
-
-@register.inclusion_tag('polls/block_poll_detail.html', takes_context=True)
-def polls_frontpage(context):
-	return {
+	ctx = {
 		'polls': Poll.objects.all()[:1],
 		'request': context['request'],
 		'user': context['user'],
 	}
+	return render_to_string('polls/block_poll_detail.html', ctx)
