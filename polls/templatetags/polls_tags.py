93490d13d94be21e4e6f5264ff446c0cb8f6f9cc
Miroslav Bendík <miroslav.bendik@gmail.com>
2014-05-01 23:18:38 +0200

    Oprava dvojitého escapovania pod jinja2.

--- polls/templatetags/polls_tags.py
+++ polls/templatetags/polls_tags.py
@@ -3,6 +3,7 @@ from django import template
 from django.template.loader import render_to_string
 from django_jinja import library
 from jinja2 import contextfunction
+from django.utils.safestring import mark_safe
 
 from polls.models import Poll
 
@@ -20,4 +21,4 @@ def polls_frontpage(context):
 		'request': context['request'],
 		'user': context['user'],
 	}
-	return render_to_string('polls/block_poll_detail.html', ctx)
+	return mark_safe(render_to_string('polls/block_poll_detail.html', ctx))
