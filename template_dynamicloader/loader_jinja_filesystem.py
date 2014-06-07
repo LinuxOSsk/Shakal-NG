7578dacbf77b84377b373ac932fbb2465f2042a5
Miroslav Bendík <miroslav.bendik@gmail.com>
2014-06-07 15:09:24 +0200

    Oprava poradia hľadania šablón.

--- template_dynamicloader/loader_jinja_filesystem.py
+++ template_dynamicloader/loader_jinja_filesystem.py
@@ -8,7 +8,7 @@ from django.template.loaders import app_directories
 
 class JinjaLoader(DynamicLoaderMixin, FileSystemLoader):
 	def __init__(self):
-		super(JinjaLoader, self).__init__(app_directories.app_template_dirs + tuple(settings.TEMPLATE_DIRS))
+		super(JinjaLoader, self).__init__(tuple(settings.TEMPLATE_DIRS) + app_directories.app_template_dirs)
 		self.template_source_cache = {}
 
 	def get_source(self, environment, template):
