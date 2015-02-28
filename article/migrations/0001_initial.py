74562c96387465be96d7051b664b04d188e8472c
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-02-28 12:00:38 +0100

    Pridaný migrate skript pre article

---
+++ article/migrations/0001_initial.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+import django.db.models.deletion
+from django.conf import settings
+import autoimagefield.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255, verbose_name='n\xe1zov')),
+                ('slug', models.SlugField(unique=True, verbose_name='skratka URL')),
+                ('perex', models.TextField(help_text='Text na titulnej str\xe1nke', verbose_name='perex')),
+                ('annotation', models.TextField(help_text='Text pred telom \u010dl\xe1nku', verbose_name='anot\xe1cia')),
+                ('content', models.TextField(verbose_name='obsah')),
+                ('authors_name', models.CharField(max_length=255, verbose_name='meno autora')),
+                ('pub_time', models.DateTimeField(default=django.utils.timezone.now, verbose_name='\u010das publik\xe1cie')),
+                ('updated', models.DateTimeField(verbose_name='aktualizovan\xe9', editable=False)),
+                ('published', models.BooleanField(default=False, verbose_name='publikovan\xe9')),
+                ('top', models.BooleanField(default=False, verbose_name='hodnotn\xfd \u010dl\xe1nok')),
+                ('image', autoimagefield.fields.AutoImageField(upload_to='article/thumbnails', null=True, verbose_name='obr\xe1zok', blank=True)),
+                ('author', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, verbose_name='autor', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+                'verbose_name': '\u010dl\xe1nok',
+                'verbose_name_plural': '\u010dl\xe1nky',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, verbose_name='n\xe1zov')),
+                ('slug', models.SlugField(unique=True, verbose_name='skratka URL')),
+                ('description', models.TextField(verbose_name='popis')),
+            ],
+            options={
+                'verbose_name': 'kateg\xf3ria',
+                'verbose_name_plural': 'kateg\xf3rie',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='category',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, verbose_name='kateg\xf3ria', to='article.Category'),
+            preserve_default=True,
+        ),
+    ]
