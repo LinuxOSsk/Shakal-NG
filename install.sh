3e8a7b7db0ea6255a6b03d3e43d0f4904f5009dd
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-05-09 15:02:42 +0200

    Aktualizovaný inštalátor

--- install.sh
+++ install.sh
@@ -1,45 +1,66 @@
 #!/bin/sh
 
+mkdir -p shakal
+cat << 'EOF' > shakal/Makefile
+.PHONY: all cimpilesprites migrate
+
 PYTHON=python2.7
+VENV_PYTHON=venv/bin/python
+DJANGO_MANAGE=cd shakal&&DJANGO_SETTINGS_MODULE=web.settings_local ../venv/bin/django-admin
 
-if which npm > /dev/null
-	then echo "Node.js .. OK"
-else
-	echo "No Node.js"
-	exit
-fi
+all: localinstall
 
-if which git > /dev/null
-	then echo "git .. OK"
-else
-	echo "No git"
-	exit
-fi
+.stamp_downloaded:
+	git clone --recursive https://github.com/mireq/Shakal-NG.git shakal
+	@touch .stamp_downloaded
 
-if which $PYTHON > /dev/null
-	then echo "${PYTHON} .. OK"
-else
-	echo "No ${PYTHON}"
-	exit
-fi
+.stamp_virtualenv: .stamp_downloaded
+	wget https://raw.github.com/pypa/virtualenv/master/virtualenv.py -O virtualenv.py
+	${PYTHON} virtualenv.py venv --no-setuptools --no-pip -p ${PYTHON}
+	rm virtualenv.py*
+	@touch .stamp_virtualenv
 
-mkdir shakal
-git clone https://github.com/mireq/Shakal-NG.git shakal/shakal-src
-mv -f shakal/shakal-src/* shakal
-mv -f shakal/shakal-src/.git shakal
-mv -f shakal/shakal-src/.gitignore shakal
-rmdir shakal/shakal-src
-wget https://raw.github.com/pypa/virtualenv/master/virtualenv.py -O shakal/virtualenv.py
-$PYTHON shakal/virtualenv.py --distribute shakal/venv -p $PYTHON
-. shakal/venv/bin/activate
-pip install -r shakal/requirements.txt
-cd shakal
-cd venv
-npm -l i less
-cd bin
-ln -s ../node_modules/less/bin/lessc .
-cd ../..
-python manage.py compilemessages
-python manage.py syncdb
-python manage.py compress_images
-python manage.py compress_less
+.stamp_setuptools: .stamp_virtualenv
+	wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py -O get-pip.py
+	${VENV_PYTHON} get-pip.py
+	rm get-pip.py*
+	@touch .stamp_setuptools
+
+.stamp_requirements: .stamp_setuptools
+	venv/bin/pip install -r shakal/requirements.txt
+	venv/bin/pip install -r shakal/requirements.local.txt
+	touch .stamp_requirements
+
+.stamp_settings: .stamp_requirements
+	cp shakal/web/settings_sample.py shakal/web/settings_local.py
+	@touch .stamp_settings
+
+compilesprites: .stamp_settings
+	${DJANGO_MANAGE} compilesprites
+
+migrate: .stamp_settings
+	${DJANGO_MANAGE} migrate
+
+runserver: .stamp_superuser
+	${DJANGO_MANAGE} runserver
+
+update: .stamp_settings
+	cd shakal; git pull; git submodule sync --recursive
+	${DJANGO_MANAGE} compilesprites
+	${DJANGO_MANAGE} migrate
+
+.stamp_superuser: .stamp_settings
+	${DJANGO_MANAGE} compilesprites
+	${DJANGO_MANAGE} migrate
+	${DJANGO_MANAGE} createsuperuser
+	@touch .stamp_superuser
+
+localinstall: .stamp_superuser
+	@echo "================================================"
+	@echo "Inštalácia prebehla úspešne"
+	@echo "Pre spustenie zadajte: cd shakal; make runserver"
+	@echo "Aktualizácia: cd shakal; make update"
+	@echo "================================================"
+EOF
+
+make -C shakal
