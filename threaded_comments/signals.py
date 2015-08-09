2202e894231b6ce48b9e9b80e402941990a87942
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-08-09 12:02:02 +0200

    Vrátené signály

--- threaded_comments/signals.py
+++ threaded_comments/signals.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.dispatch import Signal
-
 
 comment_was_flagged = Signal(providing_args=["comment", "flag", "created", "request"])
--- threaded_comments/utils.py
+++ threaded_comments/utils.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import transaction
 from django.db.models import Count, Max
 
+from . import signals
 from .models import CommentFlag, RootHeader, Comment
 
 
