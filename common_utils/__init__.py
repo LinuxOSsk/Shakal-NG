9f466d13d92f6bb519de985cb949a7dcffcf27fe
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-09-13 11:53:03 +0200

    Odstránené monkey patche

--- common_utils/__init__.py
+++ common_utils/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import os
 
 import inspect
@@ -7,7 +9,7 @@ from django import template
 from .middlewares.ThreadLocal import get_current_request
 
 
-def process_template_args(rawparams, context = None):
+def process_template_args(rawparams, context=None):
 	args = []
 	for param in rawparams:
 		pos = param.find('=')
@@ -19,7 +21,7 @@ def process_template_args(rawparams, context = None):
 	return args
 
 
-def process_template_kwargs(rawparams, context = None):
+def process_template_kwargs(rawparams, context=None):
 	kwargs = {}
 	for param in rawparams:
 		paramname = param
@@ -81,44 +83,3 @@ def get_default_manager(obj):
 
 def reload_model(obj):
 	return get_default_manager(obj.__class__).get(pk=obj.pk)
-
-
-#def monkey_patch_safestring():
-#	from django.utils.safestring import SafeData
-#	SafeData.__html__ = lambda self: self
-#
-#	from jinja2 import escape
-#	from django.forms import BaseForm, Media
-#	from django.forms.forms import BoundField
-#	from django.forms.formsets import BaseFormSet
-#	from django.forms.utils import ErrorDict, ErrorList
-#
-#	for cls in (BaseForm, Media, BoundField, BaseFormSet, ErrorDict, ErrorList):
-#		cls.__html__ = lambda self: escape(unicode(self))
-#
-#
-#monkey_patch_safestring()
-#
-#
-#def monkey_patch_jinja2_getattr():
-#	from jinja2.environment import Environment
-#
-#	def env_getattr(self, obj, attribute):
-#		try:
-#			return Environment.old_getattr(self, obj, attribute)
-#		except ObjectDoesNotExist as exc:
-#			return self.undefined(obj=obj, name=attribute, hint=unicode(exc))
-#
-#	def env_getitem(self, obj, attribute):
-#		try:
-#			return Environment.old_getitem(self, obj, attribute)
-#		except ObjectDoesNotExist as exc:
-#			return self.undefined(obj=obj, name=attribute, hint=unicode(exc))
-#
-#	Environment.old_getattr = Environment.getattr
-#	Environment.old_getitem = Environment.getitem
-#	Environment.getattr = env_getattr
-#	Environment.getitem = env_getitem
-#
-#
-#monkey_patch_jinja2_getattr()
