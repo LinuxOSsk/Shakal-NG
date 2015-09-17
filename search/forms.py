0267c3df3f0ad0eb9335ef4b30d2a6b85dc7a6a4
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-09-17 13:27:06 +0200

    Zoradenie tabuliek vo vyhľadávaní podľa modelu

--- search/forms.py
+++ search/forms.py
@@ -8,3 +8,4 @@ class SearchForm(HighlightedModelSearchForm):
 	def __init__(self, *args, **kwargs):
 		super(SearchForm, self).__init__(*args, **kwargs)
 		self.fields['models'].label = 'Hľadať v'
+		self.fields['models'].choices.sort(key=lambda x: unicode(x[0]))
