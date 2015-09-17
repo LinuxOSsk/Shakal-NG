56dd87b0a026d2ca53625ed3f0a443900f371a62
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-09-17 15:40:05 +0200

    Lazy loading ankety

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
-from django.contrib.auth.decorators import login_required
+from __future__ import unicode_literals
 
-from .views import PollDetail, PollList, PollCreate, PollPost
+from django.conf.urls import patterns, url
 
 
 class Patterns(object):
@@ -13,10 +12,10 @@ class Patterns(object):
 	@property
 	def urls(self):
 		pat = patterns('polls.views',
-			url(r'^detail/(?P<slug>[-\w]+)/$', PollDetail.as_view(), name="detail-by-slug"),
-			url(r'^(?:(?P<page>\d+)/)?$', PollList.as_view(), name='list'),
-			url(r'^post/(?P<pk>\d+)/$', PollPost.as_view(), name='post'),
-			url(r'^vytvorit/$', login_required(PollCreate.as_view()), name='create'),
+			url(r'^detail/(?P<slug>[-\w]+)/$', 'PollDetail', name='detail-by-slug'),
+			url(r'^(?:(?P<page>\d+)/)?$', 'PollList', name='list'),
+			url(r'^post/(?P<pk>\d+)/$', 'PollPost', name='post'),
+			url(r'^vytvorit/$', 'PollCreate', name='create'),
 		)
 		return (pat, self.app_name, self.name)
 
