c28c31b4137cadc3c22963a4fed3d216f057f6cf
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-10-25 10:04:25 +0100

    Oprava náhľadu vo ff

--- static/js/richeditor/editor.js
+++ static/js/richeditor/editor.js
@@ -59,7 +59,8 @@ var SimpleEditorHtml = function(element, options) {
 	};
 
 	var updatePreviewCss = function() {
-		var inlineCss = preview.documentElement.getElementsByTagName('STYLE')[0];
+		var doc = preview.documentElement ? preview.documentElement : (preview.contentDocument ? preview.contentDocument : preview.contentWindow.document)
+		var inlineCss = doc.getElementsByTagName('STYLE')[0];
 		inlineCss.innerHTML = options.tags.unsupported.join(', ') + '{ background-color: #ff9999 !important; border: 1px solid red !important; }';
 	};
 
