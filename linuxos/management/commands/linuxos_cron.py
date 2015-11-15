8914e34924ce6a40bf29efcc27b269c04a9b3151
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-11-15 20:04:43 +0100

    Aktualizácia počtu komentárov

--- linuxos/management/commands/linuxos_cron.py
+++ linuxos/management/commands/linuxos_cron.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.core.management.base import BaseCommand
 
-from ...tasks import delete_old_attachments
+from ...tasks import delete_old_attachments, update_user_ratings
 
 
 #import datetime
@@ -29,6 +29,7 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **kwargs):
 		delete_old_attachments()
+		update_user_ratings()
 		#self.update_user_ratings()
 		#self.delete_old_events()
 
@@ -45,14 +46,6 @@ class Command(BaseCommand):
 	#	ratings = [dict(zip(columns, r)) for r in ratings]
 	#	ratings = dict([(r['user'], r) for r in ratings])
 
-	#	user_comments = Comment.objects.filter(user_id__isnull = False, is_removed = False, is_public = True).values('user_id').annotate(Count('pk')).order_by('user').values_list('user_id', 'pk__count')
-	#	user_comments_changed = filter(lambda c: c[0] not in ratings or c[1] != ratings[c[0]]['comments'], user_comments)
-	#	for user_id, comment_count in user_comments_changed:
-	#		rating, created = UserRating.objects.get_or_create(user_id = user_id)
-	#		rating.comments = comment_count
-	#		rating.save()
-	#	del(user_comments)
-	#	del(user_comments_changed)
 
 	#	user_articles = Article.objects.filter(author_id__isnull = False).values('author_id').annotate(Count('pk')).values_list('author_id', 'pk__count')
 	#	user_articles_changed = filter(lambda c: c[0] not in ratings or c[1] != ratings[c[0]]['articles'], user_articles)
--- linuxos/tasks.py
+++ linuxos/tasks.py
@@ -3,12 +3,37 @@ from __future__ import unicode_literals
 
 from datetime import timedelta
 
+from django.db.models import Count
 from django.utils import timezone
 
+from accounts.models import UserRating
 from attachment.models import TemporaryAttachment
+from comments.models import Comment
+
+
+def _update_rating_data(new_ratings, field_name):
+	current_ratings = dict(UserRating.objects.values_list('user_id', field_name))
+	for user_id, count in new_ratings:
+		if count != current_ratings.get(user_id, 0):
+			UserRating.objects.update_or_create(user_id=user_id, defaults={field_name: count})
 
 
 def delete_old_attachments():
-	old_attachments = TemporaryAttachment.objects.filter(created__lt=timezone.now() - timedelta(1))
+	old_attachments = (TemporaryAttachment.objects
+		.filter(created__lt=timezone.now() - timedelta(1)))
 	for old_attachment in old_attachments:
 		old_attachment.delete()
+
+
+def update_user_ratings():
+	update_user_ratings_comments()
+
+
+def update_user_ratings_comments():
+	new_ratings = (Comment.objects
+		.filter(user_id__isnull=False, is_removed=False, is_public=True)
+		.order_by('user_id')
+		.values('user_id')
+		.annotate(comment_count=Count('pk'))
+		.values_list('user_id', 'comment_count'))
+	_update_rating_data(new_ratings, 'comments')
