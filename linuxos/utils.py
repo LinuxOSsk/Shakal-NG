ad8460c51c6c3083f751965abb81053a51859d28
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-11-22 17:11:51 +0100

    Počet nvých príspevkov podľa typu obsahu, #18

--- linuxos/utils.py
+++ linuxos/utils.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from bisect import bisect_left
 from django.apps import apps
 from django.core.cache import caches
+
 from common_utils import get_meta
 
 
@@ -17,8 +19,8 @@ def last_objects():
 		for app_label, model_name in MODELS:
 			last = (apps.get_model(app_label, model_name).objects
 				.order_by('-created')
-				.values_list('created')[:99])
-			objects_cache[(app_label, model_name)] = last
+				.values_list('created', flat=True)[:99])
+			objects_cache[(app_label, model_name)] = list(reversed(last))
 		default_cache.set('last_objects', objects_cache)
 	return objects_cache
 
@@ -28,3 +30,13 @@ def clear_cache(sender, **kwargs):
 	if (opts.app_label, opts.model_name) not in MODELS:
 		return
 	default_cache.delete('last_objects')
+
+
+def count_new(last_display_time):
+	counts = {}
+	for model, dates in last_objects().iteritems():
+		if model in last_display_time:
+			counts[model] = len(dates) - bisect_left(dates, last_display_time[model])
+		else:
+			counts[model] = None
+	return counts
