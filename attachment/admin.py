bdeff93a766714144e4609e13dd277509c5ba183
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-11-29 11:41:51 +0100

    Odstránený kus prebytočného kódu, refs #19

--- attachment/admin.py
+++ attachment/admin.py
@@ -3,21 +3,21 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
-from django.utils.translation import ugettext_lazy as _
 
 from attachment.models import Attachment
 
 
 class AttachmentInline(GenericTabularInline):
 	model = Attachment
-	verbose_name = _('attachment')
-	verbose_name_plural = _('attachments')
+	verbose_name = 'príloha'
+	verbose_name_plural = 'prílohy'
 	ct_field = 'content_type'
 	ct_fk_field = 'object_id'
 	exclude = ('size', )
 	can_delete = True
-	template = "admin/edit_inline/attachments.html"
-	extra = 1
+	template = 'admin/edit_inline/attachments.html'
+	readonly_fields = ('attachment',)
+	extra = 0
 
 	def get_queryset(self, request):
 		return super(AttachmentInline, self).get_queryset(request).select_related('attachmentimage')
