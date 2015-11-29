36a8f3d7a0796baf29addfe61f1dd33e0dd16916
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-11-29 19:13:55 +0100

    Pridané odkazy na dokumentáciu

--- static/js/attachment-upload.js
+++ static/js/attachment-upload.js
@@ -1,8 +1,22 @@
 (function(_) {
 "use strict";
 
+// http://stackoverflow.com/questions/14069421/show-an-image-preview-before-upload
+// http://www.sitepoint.com/html5-javascript-file-upload-progress-bar/
+// https://developer.mozilla.org/en-US/docs/Using_files_from_web_applications
+
+var createUploader = function(element) {
+	_.addClass(uploadElement, 'element');
+};
+
+
 var register = function(root) {
+	if (!_.checkFeatures(["ajax", "drop", "file"])) {
+		return;
+	}
+
 	_.forEach(_.cls(root, 'attachment-upload'), function(uploadElement) {
+		createUploader(uploadElement);
 	});
 };
 
