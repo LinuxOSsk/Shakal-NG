418c4268ab8fbbf58e8dd7d6d8a51972994d5104
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-12-13 15:15:02 +0100

    Nastavenie authors_name ja keď je commit=false

--- common_utils/forms.py
+++ common_utils/forms.py
@@ -17,7 +17,7 @@ class AuthorsNameFormMixin(object):
 	def __init__(self, *args, **kwargs):
 		super(AuthorsNameFormMixin, self).__init__(*args, **kwargs)
 		request = get_current_request()
-		authors_name_field = self.get_authors_name_field()
+		authors_name_field = self.authors_name_field
 		if request.user.is_authenticated():
 			if authors_name_field in self.fields:
 				del self.fields[authors_name_field]
@@ -31,23 +31,23 @@ class AuthorsNameFormMixin(object):
 				self.fields[authors_name_field].required = True
 				self.fields[authors_name_field].validators = [USERNAME_VALIDATOR]
 
-	def get_authors_name_field(self):
-		return self.authors_name_field
-
 	def clean(self):
+		authors_name_field = self.authors_name_field
 		cleaned_data = super(AuthorsNameFormMixin, self).clean()
-		authors_name = self.get_authors_name_field()
-		if authors_name in cleaned_data:
-			if get_user_model().objects.filter(username=cleaned_data[authors_name]).exists():
-				self.add_error(authors_name, 'Používateľ s takýmto menom už existuje')
+		if self.authors_name_field in cleaned_data:
+			duplicat_username = (get_user_model().objects
+				.filter(username=cleaned_data[authors_name_field])
+				.exists())
+			if duplicat_username:
+				self.add_error(authors_name_field, 'Používateľ s takýmto menom už existuje')
 		return cleaned_data
 
 	def save(self, commit=True):
 		obj = super(AuthorsNameFormMixin, self).save(commit=False)
+		request = get_current_request()
+		if request.user.is_authenticated():
+			username = get_current_request().user.get_full_name()
+			setattr(obj, self.authors_name_field, username)
 		if commit:
-			authors_name_field = self.get_authors_name_field()
-			cleaned_data = self.cleaned_data
-			if not authors_name_field in cleaned_data:
-				setattr(obj, authors_name_field, get_current_request().user.get_full_name())
 			obj.save()
 		return obj
