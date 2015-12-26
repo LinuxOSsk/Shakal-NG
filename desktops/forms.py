9c54517f015c4412f161904417ecb113c8ba3f41
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-12-26 17:42:22 +0100

    Maximálna veľkosť desktopu je 2MB

--- desktops/forms.py
+++ desktops/forms.py
@@ -4,9 +4,12 @@ from __future__ import unicode_literals
 from django import forms
 
 from .models import Desktop
+from attachment.fields import AttachmentField, AttachmentWidget
 
 
 class DesktopCreateForm(forms.ModelForm):
+	image = AttachmentField(label='obrázok', widget=AttachmentWidget(attrs={'max_size': 1024 * 1024 * 2}))
+
 	class Meta:
 		model = Desktop
 		fields = ('title', 'image', 'original_text')
