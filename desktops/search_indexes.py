877a292eedc4cbc7a7d8acc763b1f1440937a169
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-12-27 16:59:52 +0100

    Pridaný vyhľadávací index pre desktopy, refs #26

---
+++ desktops/search_indexes.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from haystack import indexes
+
+from .models import Desktop
+
+
+class DesktopIndex(indexes.SearchIndex, indexes.Indexable):
+	created = indexes.DateTimeField(model_attr='pub_time')
+	updated = indexes.DateTimeField(model_attr='updated')
+	title = indexes.CharField(model_attr='title')
+	text = indexes.CharField(document=True, use_template=True)
+
+	def get_updated_field(self):
+		return 'updated'
+
+	def get_model(self):
+		return Desktop
+
+	def index_queryset(self, using=None):
+		return self.get_model().objects.all()
