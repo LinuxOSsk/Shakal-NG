a6860a4d3e81887c247f8bb41a9b4ff6561fef8b
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-01-03 15:10:03 +0100

    Vynútené https

--- linuxos/templatetags/linuxos.py
+++ linuxos/templatetags/linuxos.py
@@ -94,13 +94,18 @@ def labelize_content_type(content_type):
 @register.simple_tag(takes_context=True)
 def get_base_uri(context):
 	if 'request' in context:
-		return context['request'].build_absolute_uri('/')[:-1]
+		request = context['request']
 	else:
 		request = get_current_request()
-		if request:
-			return request.build_absolute_uri('/')[:-1]
-		else:
-			return ''
+	if request:
+		url = request.build_absolute_uri('/')[:-1]
+		if url.startswith('http://'):
+			url = 'https://' + url[7:]
+		return url
+	if 'request' in context:
+		return context['request'].build_absolute_uri('/')[:-1]
+	else:
+		return ''
 
 
 @library.global_function
