388538d91a0e17a52bddc6f57d82e7f0da98635c
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-01-06 14:09:11 +0100

    Redirect na prehľad fóra

--- forum/urls.py
+++ forum/urls.py
@@ -2,9 +2,11 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import patterns, url
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = patterns('forum.views',
+	url(r'^$', RedirectView.as_view(pattern_name='forum:overview', permanent=False)),
 	url(r'^prehlad/(?:(?P<page>\d+)/)?$', 'TopicListView', name='overview'),
 	url(r'^pridat/$', 'TopicCreateView', name='create'),
 	url(r'^(?P<pk>\d+)/$', 'TopicDetailView', name='topic-detail'),
