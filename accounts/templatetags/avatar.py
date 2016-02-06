96281307defb975054bc46bf3ec499abc47f24c1
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-02-06 19:25:51 +0100

    Zobrazenie avatarov podľa IP, refs #71

--- accounts/templatetags/avatar.py
+++ accounts/templatetags/avatar.py
@@ -7,6 +7,8 @@ from django.conf import settings
 from django.utils.html import escape
 from django_jinja import library
 
+from accounts.utils import generated_avatar
+
 
 GRAVATAR_URL_PREFIX = getattr(settings, "GRAVATAR_URL_PREFIX", "//www.gravatar.com/")
 GRAVATAR_DEFAULT_IMAGE = getattr(settings, "GRAVATAR_DEFAULT_IMAGE", "")
@@ -26,3 +28,18 @@ def avatar_for_user(user, size=GRAVATAR_DEFAULT_SIZE):
 		if avatar:
 			return avatar.url
 	return gravatar_for_email(user.email, size)
+
+
+@library.global_function
+def prefetch_avatars_for_ip(*object_lists):
+	icon_cache = {}
+
+	for object_list in object_lists:
+		for obj in object_list:
+			ip = obj.ip_address
+			if ip:
+				if not ip in icon_cache:
+					icon_cache[ip] = generated_avatar(ip)
+				obj.ip_address_avatar = icon_cache[ip]
+
+	return ''
--- accounts/utils.py
+++ accounts/utils.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import hashlib
+import os
+
+from PIL import Image, ImageFilter
 from django.apps import apps
+from django.conf import settings
 from django.core.cache import caches
 from django.utils import timezone
 from django.utils.dateparse import parse_datetime
@@ -16,6 +21,27 @@ MODELS = [
 	'news.news',
 	'wiki.page',
 ]
+
+
+AVATAR_IMAGES = {
+	'male': (
+		('background', 5),
+		('face', 4),
+		('clothes', 66),
+		('mouth', 26),
+		('hair', 37),
+		('eye', 33),
+	),
+	'female': (
+		('background', 5),
+		('face', 4),
+		('clothes', 59),
+		('mouth', 17),
+		('head', 34),
+		('eye', 53),
+	),
+}
+
 
 default_cache = caches['default']
 
@@ -87,3 +113,45 @@ def get_count_new(user):
 	last_visited = user_settings.get('last_visited', {})
 	visited_items = user_settings.get('visited_items', {})
 	return count_new(last_visited, visited_items)
+
+
+def generated_avatar(data):
+	hash_str = hashlib.md5(data).hexdigest()
+
+	avatar_dirname = os.path.join('CACHE', 'avatars', hash_str[-2:])
+	avatar_filename = hash_str[:8] + '.png'
+	if os.path.exists(os.path.join(settings.STATICFILES_DIRS[0], avatar_dirname, avatar_filename)):
+		return os.path.join(avatar_dirname, avatar_filename)
+
+	data_hash = int(hash_str, 16)
+	sex = 'male' if bool(data_hash % 4) else 'female'
+	data_hash = data_hash / 4
+	avatar_recipe = AVATAR_IMAGES[sex]
+
+	img = None
+	for name, count in avatar_recipe:
+		imgnum = (data_hash % count) + 1
+		filename = os.path.join(os.path.dirname(__file__), '8biticon', '8bit-client', 'img', sex, name + str(imgnum) + '.png')
+		data_hash = data_hash / count
+		if img is None:
+			img = Image.open(filename).convert('RGB')
+			img = img.filter(ImageFilter.GaussianBlur(25))
+			img = img.resize((48, 48), Image.NEAREST)
+		else:
+			past = Image.open(filename)
+			past = past.resize((40, 40), Image.NEAREST)
+			img.paste(past, (4, 7, 44, 47), past)
+
+
+	try:
+		os.makedirs(os.path.join(settings.STATICFILES_DIRS[0], avatar_dirname))
+	except OSError:
+		pass
+	img.save(os.path.join(settings.STATICFILES_DIRS[0], avatar_dirname, avatar_filename))
+	if hasattr(settings, 'STATIC_ROOT'):
+		try:
+			os.makedirs(os.path.join(settings.STATIC_ROOT, avatar_dirname))
+		except OSError:
+			pass
+		img.save(os.path.join(settings.STATIC_ROOT, avatar_dirname, avatar_filename))
+	return os.path.join(avatar_dirname, avatar_filename)
