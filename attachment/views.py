d9880c97795c2f982c32fbdb1508e20626b1af45
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-03-01 20:12:09 +0100

    Opravené API pre upload súborov, refs #78

--- attachment/views.py
+++ attachment/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from os.path import basename
 
+from easy_thumbnails.files import get_thumbnailer
+
 
 class AttachmentManagementMixin(object):
 	def get_attachments_list(self, qs):
@@ -15,10 +17,12 @@ class AttachmentManagementMixin(object):
 				'filesize': attachment.size,
 			}
 			if hasattr(attachment, 'attachmentimage'):
+				thumbnailer = get_thumbnailer(attachment.attachment.name)
+				options = {'size': (256, 256)}
 				attachment_data['width'] = attachment.attachmentimage.width
 				attachment_data['height'] = attachment.attachmentimage.height
 				attachment_data['thumbnails'] = {
-					'standard': attachment.attachmentimage.attachment_standard.url
+					'standard': thumbnailer.get_thumbnail(options).url
 				}
 			attachments.append(attachment_data)
 		return attachments
