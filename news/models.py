15210b9c39f309e393e7b4d16fc2b511aa53b9ff
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-03-15 20:55:18 +0100

    Pridaný generický vzťah na poznámky, refs #74

--- news/models.py
+++ news/models.py
@@ -77,6 +77,7 @@ class News(TimestampModelMixin, models.Model):
 	comments_header = GenericRelation(RootHeader)
 	attachments = GenericRelation(Attachment)
 	comments = GenericRelation(Comment)
+	notes = GenericRelation('notes.Note')
 
 	content_fields = ('original_short_text', 'original_long_text',)
 
