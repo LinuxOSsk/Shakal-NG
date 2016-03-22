7c7614f2cd1fc067d78b8eb0cc02b01e21b4c042
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-03-22 18:28:16 +0100

    Niektoré polia sa neprekladajú

--- accounts/admin_forms.py
+++ accounts/admin_forms.py
@@ -5,7 +5,6 @@ import re
 
 from django import forms
 from django.contrib.auth.forms import UserChangeForm as OrigUserChangeForm, UserCreationForm as OrigUserCreationForm
-from django.utils.translation import ugettext_lazy as _
 
 from common_utils.admin_widgets import DateTimeInput, EnclosedInput
 
@@ -16,8 +15,8 @@ def get_username_field():
 		max_length=30,
 		min_length=3,
 		regex=re.compile(r'^([\w]+[ ]?)*[\w]$', re.UNICODE),
-		help_text=_('Povinné. Dĺžka 3 - 30 znakov.'),
-		error_message=_('Toto pole môže obsahovať maximálne jednu medzeru v strede.'))
+		help_text='Povinné. Dĺžka 3 - 30 znakov.',
+		error_message='Toto pole môže obsahovať maximálne jednu medzeru v strede.')
 
 
 class UserCreationForm(OrigUserCreationForm):
