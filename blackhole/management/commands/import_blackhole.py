b9f1ba0b4df76d93c52d9d764ccde50e1b0657a9
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-04-23 09:15:11 +0200

    Prevod formátov na naše formáty, refs #85

--- blackhole/management/commands/import_blackhole.py
+++ blackhole/management/commands/import_blackhole.py
@@ -6,10 +6,18 @@ from collections import namedtuple
 from django.core.management.base import BaseCommand
 from django.db import connections
 from django.utils.functional import cached_property
-from common_utils.asciitable import NamedtupleTablePrinter
+#from common_utils.asciitable import NamedtupleTablePrinter
 
 
 FilterFormat = namedtuple('FilterFormat', ['format', 'name'])
+
+
+FORMATS_TRANSLATION = {
+	'Filtered HTML': 'html',
+	'PHP code': 'html',
+	'Full HTML': 'raw',
+	'No HTML': 'text',
+}
 
 
 class Command(BaseCommand):
@@ -24,8 +32,8 @@ class Command(BaseCommand):
 	def filter_formats(self):
 		cursor = self.db_cursor()
 		cursor.execute('SELECT format, name FROM filter_formats')
-		return tuple(FilterFormat(*row) for row in cursor.fetchall())
+		formats = tuple(FilterFormat(*row) for row in cursor.fetchall())
+		return {f.format: FORMATS_TRANSLATION[f.name] for f in formats}
 
 	def handle(self, *args, **options):
-		print('filter_formats')
-		print(NamedtupleTablePrinter(self.filter_formats, FilterFormat).render())
+		print(self.filter_formats)
