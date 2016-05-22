06edd75b8ec69e604d257bd26806d495f5726dea
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-05-22 17:56:37 +0200

    Pridané vyhľadávacie indexy pre články z blackhole, refs #85

---
+++ blackhole/search_indexes.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from haystack import indexes
+
+from .models import Node
+
+
+class NodeIndex(indexes.SearchIndex, indexes.Indexable):
+	created = indexes.DateTimeField(model_attr='created')
+	updated = indexes.DateTimeField(model_attr='updated')
+	title = indexes.CharField(model_attr='title')
+	text = indexes.CharField(document=True, use_template=True)
+
+	def get_updated_field(self):
+		return 'updated'
+
+	def get_model(self):
+		return Node
+
+	def index_queryset(self, using=None):
+		return self.get_model().objects.filter(node_type='story')
