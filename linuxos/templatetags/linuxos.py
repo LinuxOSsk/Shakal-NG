dc7991ded8060f0fd1d715b953c02dc67fbac79e
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-06-03 14:29:57 +0200

    Nofollow pre odkazy na profily

--- linuxos/templatetags/linuxos.py
+++ linuxos/templatetags/linuxos.py
@@ -46,7 +46,7 @@ def humandatetime(value, default=''):
 @register.simple_tag
 def user_link(user_object, username):
 	if user_object:
-		return format_html('<a class="url fn" href="{0}">{1}</a>', user_object.get_absolute_url(), smart_unicode(user_object))
+		return format_html('<a class="url fn" href="{0}" rel="nofollow">{1}</a>', user_object.get_absolute_url(), smart_unicode(user_object))
 	else:
 		return escape(username)
 
