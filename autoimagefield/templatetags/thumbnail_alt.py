eaf7b7bac99ae560625e0a6f07bbe590c9d62ebc
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-02-26 11:48:50 +0100

    Ošetrenie chyby pri generovaní náhľadov

--- autoimagefield/templatetags/thumbnail_alt.py
+++ autoimagefield/templatetags/thumbnail_alt.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import logging
+
+from django.conf import settings
 from django_jinja import library
 
 from autoimagefield.utils import thumbnail as create_thumbnail
 
 
+logger = logging.getLogger('shakal')
+
+
 @library.global_function
 def thumbnail(source_url, **kwargs):
-	return create_thumbnail(source_url, **kwargs).url
+	try:
+		return create_thumbnail(source_url, **kwargs).url
+	except Exception: #pylint: disable=broad-except
+		if getattr(settings, 'THUMBNAIL_DEBUG', False):
+			raise
+		else:
+			logger.exception("Thumbnail not created")
