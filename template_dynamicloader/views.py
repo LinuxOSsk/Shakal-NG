57c5cfc2fdd37e743829555afff60d46b1a43c2e
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-05-07 18:15:22 +0200

    Oprava presmerovania pri nastavení šablóny

--- template_dynamicloader/views.py
+++ template_dynamicloader/views.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.decorators.http import require_POST
@@ -13,7 +12,7 @@ from .utils import switch_template
 @require_POST
 def change(request):
 	form = ChangeTemplateHiddenForm(request.POST)
-	response = HttpResponseRedirect(request.POST.get('next', reverse(settings.HOME_URL)))
+	response = HttpResponseRedirect(request.POST.get('next', reverse('home')))
 	if form.is_valid() and 'change_style' in request.POST:
 		switch_template(response, **form.cleaned_data)
 	return response
