0b6d8998a97e35c93d36c1c973007342a66163cd
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-05-07 17:29:27 +0200

    To je asi slepá ulička

--- template_dynamicloader/backend.py
+++ template_dynamicloader/backend.py
@@ -2,10 +2,12 @@
 from __future__ import unicode_literals
 
 from django.template.engine import Engine
-from django_jinja.backend import Jinja2 as CoreJinja2
+from django_jinja import backend
+from .loader_filesystem import DynamicLoaderMixin
+import jinja2
 
 
-class Jinja2(CoreJinja2):
+class Jinja2(DynamicLoaderMixin, backend.Jinja2):
 	def __init__(self, params):
 		options = params.pop('ENGINE_OPTIONS')
 		dirs = list(params.get('DIRS'))
@@ -13,3 +15,9 @@ class Jinja2(CoreJinja2):
 		self.engine = Engine(dirs, app_dirs, **options)
 		params['OPTIONS']['loader'] = self.engine.template_loaders[0]
 		super(Jinja2, self).__init__(params)
+
+	def get_template(self, template_name):
+		try:
+			return super(Jinja2, self).get_template(self.get_visitors_template(template_name))
+		except:
+			return super(Jinja2, self).get_template(template_name)
