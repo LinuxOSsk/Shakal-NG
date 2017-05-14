992606b5bbe7d23acc8cddfbeeb51346875ffb6c
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-05-14 17:02:12 +0200

    Oprava testov v rich editore

--- rich_editor/tests.py
+++ rich_editor/tests.py
@@ -12,72 +12,72 @@ class ParserTest(TestCase):
 	def test_auto_paragraph(self):
 		code = """Test"""
 		self.parser.parse(code)
-		self.assertEquals(self.parser.get_output(), "<p>" + code + "</p>")
+		self.assertEqual(self.parser.get_output(), "<p>" + code + "</p>")
 
 	def test_valid_html(self):
 		code = "<p><strong>Test</strong></p>\n<pre>\nTadaaa\n\n</pre><p>Text</p>"
 		self.parser.parse(code)
-		self.assertEquals(self.parser.get_output(), "<p><strong>Test</strong></p>\n<pre>\nTadaaa\n\n</pre><p>Text</p>")
+		self.assertEqual(self.parser.get_output(), "<p><strong>Test</strong></p>\n<pre>\nTadaaa\n\n</pre><p>Text</p>")
 
 	def test_opened_tag(self):
 		code = """<p>Test"""
 		self.parser.parse(code)
-		self.assertEquals(self.parser.get_output(), code + "</p>")
+		self.assertEqual(self.parser.get_output(), code + "</p>")
 
 	def test_opened_nested_tag(self):
 		code = """<p><strong>Test</p>"""
 		self.parser.parse(code)
-		self.assertEquals(self.parser.get_output(), code[:-4] + "</strong></p>")
+		self.assertEqual(self.parser.get_output(), code[:-4] + "</strong></p>")
 
 	def test_unknown_tag(self):
 		code = """<xxx>Test</xxx>"""
 		self.parser.parse(code)
-		self.assertEquals(self.parser.get_output(), code.replace('<', '&lt;').replace('>', '&gt;'))
+		self.assertEqual(self.parser.get_output(), code.replace('<', '&lt;').replace('>', '&gt;'))
 
 	def test_attribute(self):
 		code = """<p><a href="#test">Test</a></p>"""
 		self.parser.parse(code)
-		self.assertEquals(self.parser.get_output(), code)
+		self.assertEqual(self.parser.get_output(), code)
 
 	def test_missing_attribute(self):
 		code = """<p><a>Test</a></p>"""
 		self.parser.parse(code)
-		self.assertEquals(self.parser.get_output(), """<p><a href="#">Test</a></p>""")
+		self.assertEqual(self.parser.get_output(), """<p><a href="#">Test</a></p>""")
 
 	def test_malicious_href(self):
 		code = """<p><a href="javascript:alert('XSS')">Test</a></p>"""
 		self.parser.parse(code)
-		self.assertEquals(self.parser.get_output(), """<p><a href="#">Test</a></p>""")
+		self.assertEqual(self.parser.get_output(), """<p><a href="#">Test</a></p>""")
 
 	def test_signature_nofollow(self):
 		code = """<a href="http://www.linuxos.sk">Test</a>"""
 		parser = get_parser('signature')
 		parser.parse(code)
-		self.assertEquals(parser.get_output(), """<a href="http://www.linuxos.sk" rel="nofollow">Test</a>""")
+		self.assertEqual(parser.get_output(), """<a href="http://www.linuxos.sk" rel="nofollow">Test</a>""")
 
 	def test_signature_bad_nofollow(self):
 		code = """<a href="http://www.linuxos.sk" rel="follow">Test</a>"""
 		parser = get_parser('signature')
 		parser.parse(code)
-		self.assertEquals(parser.get_output(), """<a href="http://www.linuxos.sk" rel="nofollow">Test</a>""")
+		self.assertEqual(parser.get_output(), """<a href="http://www.linuxos.sk" rel="nofollow">Test</a>""")
 
 	def test_profile_parser(self):
 		code = """<p><img src="http://www.linuxos.sk/img.png" alt="" /></p>"""
 		parser = get_parser('profile')
 		parser.parse(code)
-		self.assertEquals(parser.get_output(), code)
+		self.assertEqual(parser.get_output(), code)
 
 	def test_auto_paragraphs(self):
 		code = "Paragraph1\n\nParagraph2"
 		self.parser.parse(code)
-		self.assertEquals(self.parser.get_output(), "<p>Paragraph1</p>\n\n<p>Paragraph2</p>")
+		self.assertEqual(self.parser.get_output(), "<p>Paragraph1</p>\n\n<p>Paragraph2</p>")
 
 	def test_auto_paragraphs_between(self):
 		code = "<blockquote>BQ</blockquote>Paragraph"
 		self.parser.parse(code)
-		self.assertEquals(self.parser.get_output(), "<blockquote>BQ</blockquote><p>Paragraph</p>")
+		self.assertEqual(self.parser.get_output(), "<blockquote>BQ</blockquote><p>Paragraph</p>")
 
 	def test_attr_entity(self):
 		code = """<p><a href="http://linuxos.sk/a&amp;b">Test</a></p>"""
 		self.parser.parse(code)
-		self.assertEquals(self.parser.get_output(), """<p><a href="http://linuxos.sk/a&amp;b">Test</a></p>""")
+		self.assertEqual(self.parser.get_output(), """<p><a href="http://linuxos.sk/a&amp;b">Test</a></p>""")
