a4e4a5db7c6c446e0ca39e59965bb95f98db0c59
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-05-14 16:17:05 +0200

    Oprava uploadu bez limitu

--- attachment/utils.py
+++ attachment/utils.py
@@ -12,12 +12,12 @@ def get_available_size(content_type, uploaded_size):
 	max_size = getattr(settings, 'ATTACHMENT_MAX_SIZE', -1)
 	db_table = "{0}_{1}".format(content_type.app_label, content_type.model)
 	size_for_content = getattr(settings, 'ATTACHMENT_SIZE_FOR_CONTENT', {}).get(db_table, None)
-	# Bez limitu
-	if max_size < 0 and size_for_content < 0:
-		return -1
 	# Obsah bez limitu
 	if size_for_content is None:
 		return max_size
+	# Bez limitu
+	if max_size < 0 and size_for_content < 0:
+		return -1
 	if size_for_content < 0:
 		return -1
 	return max(size_for_content - uploaded_size, 0)
