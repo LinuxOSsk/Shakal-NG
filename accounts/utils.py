4be146b68dc836586b835006dd3b82fae59c21df
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-06-03 17:20:00 +0200

    Aktualizovaná 8biticon

--- accounts/utils.py
+++ accounts/utils.py
@@ -131,7 +131,7 @@ def generated_avatar(data):
 	img = None
 	for name, count in avatar_recipe:
 		imgnum = (data_hash % count) + 1
-		filename = os.path.join(os.path.dirname(__file__), '8biticon', '8bit-client', 'img', sex, name + str(imgnum) + '.png')
+		filename = os.path.join(os.path.dirname(__file__), '8biticon', 'public', 'static', '8bit', 'img', sex, name + str(imgnum) + '.png')
 		data_hash = data_hash // count
 		if img is None:
 			img = Image.open(filename).convert('RGB')
