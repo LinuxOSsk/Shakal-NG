2872aa9b25e9d546554963b9250f6ca11b217191
Miroslav Bendík <miroslav.bendik@gmail.com>
2018-06-24 15:01:58 +0200

    Oprava highlightu výrazov ako c++

--- linuxos/search.py
+++ linuxos/search.py
@@ -2,6 +2,8 @@
 # pylint: disable=abstract-method
 from __future__ import unicode_literals
 
+import re
+
 import xapian
 from haystack.backends import BaseEngine
 from xapian_backend import XapianSearchBackend as CoreXapianSearchBackend, XapianSearchQuery
@@ -28,6 +30,16 @@ class XapianSearchBackend(CoreXapianSearchBackend):
 			return super().parse_query(query_string)
 		except xapian.QueryParserError:
 			return xapian.Query()
+
+	@staticmethod
+	def _do_highlight(content, query, tag='em'):
+		for term in query:
+			term = term.decode('utf-8')
+			for match in re.findall('[^A-Z]+', term):  # Ignore field identifiers
+				match_re = re.compile(re.escape(match), re.I)
+				content = match_re.sub('<%s>%s</%s>' % (tag, term, tag), content)
+
+		return content
 
 
 class XapianEngine(BaseEngine):
