7e5bd1d530de14b8c14f51ee74e4a2427afdea39
Miroslav Bendík <miroslav.bendik@gmail.com>
2019-02-13 20:58:04 +0100

    Deklaratívna definícia štatistík

--- linuxos/management/commands/generate_stats.py
+++ linuxos/management/commands/generate_stats.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 import csv
+from collections import namedtuple
 from io import BytesIO
 
 from django.core.files.storage import default_storage
 from django.core.management.base import BaseCommand
 from django.db.models import Count, Q
+from django.utils import timezone
 
 from accounts.models import User
+from article.models import Article
+from blog.models import Post as BlogPost
+from comments.models import Comment
+from desktops.models import Desktop
+from forum.models import Topic
+from news.models import News
+from tweets.models import Tweet
+from wiki.models import Page as WikiPage
+
+
+ContentModel = namedtuple('ContentModel', ['model', 'label', 'author', 'username', 'date', 'agg_filter', 'reverse_name'])
+
 
 
 class CsvWriter(object):
@@ -35,24 +49,89 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		self.write_users()
 
+	def get_content_models(self):
+		now = timezone.now()
+		return (
+			ContentModel(
+				Article, 'articles',
+				author='author',
+				username='authors_name',
+				date='created',
+				agg_filter=Q(article__published=True, article__pub_time__lte=now),
+				reverse_name='article'
+			),
+			ContentModel(
+				BlogPost, 'blogs',
+				author='blog__author',
+				username=None,
+				date='created',
+				agg_filter=Q(blog__post__pub_time__lte=now),
+				reverse_name='blog__posts'
+			),
+			ContentModel(
+				Comment, 'comments',
+				author='user',
+				username='user_name',
+				date='created',
+				agg_filter=Q(comment_comments__parent__isnull=False, comment_comments__is_public=True, comment_comments__is_removed=False),
+				reverse_name='comment_comments'
+			),
+			ContentModel(
+				Desktop, 'desktops',
+				author='author',
+				username=None,
+				date='created',
+				agg_filter=None,
+				reverse_name='desktop'
+			),
+			ContentModel(
+				Topic, 'topics',
+				author='author',
+				username='authors_name',
+				date='created',
+				agg_filter=None,
+				reverse_name='topic'
+			),
+			ContentModel(
+				News, 'news',
+				author='author',
+				username='authors_name',
+				date='created',
+				agg_filter=Q(news__approved=True),
+				reverse_name='news'
+			),
+			ContentModel(
+				Tweet, 'tweets',
+				author='author',
+				username=None,
+				date='created',
+				agg_filter=None,
+				reverse_name='tweet'
+			),
+			ContentModel(
+				WikiPage, 'wiki_pages',
+				author='last_author',
+				username=None,
+				date='created',
+				agg_filter=None,
+				reverse_name='page'
+			),
+		)
+
 	def write_users(self):
+		logged_users_stats = User.objects.order_by('pk').values('username', 'pk')
+		fields = []
+		for content_model in self.get_content_models():
+			if not content_model.author or not content_model.reverse_name:
+				continue
+			count = Count(content_model.reverse_name, distinct=True, filter=content_model.agg_filter)
+			logged_users_stats = logged_users_stats.annotate(**{'count_'+content_model.label: count})
+			fields.append(content_model.label)
+
+		logged_users_stats = logged_users_stats.values_list('username', 'pk', *['count_'+label for label in fields])
+
 		writer = CsvWriter('stats/user.csv')
-		writer.write_row(['username', 'pk', 'articles', 'blogs', 'comments', 'desktops', 'topics', 'news', 'tweets', 'wkiki_pages'])
-
-		users = (User.objects
-			.annotate(
-				articles=Count('article', distinct=True),
-				blogs=Count('blog__posts', distinct=True),
-				comments=Count('comment_comments', filter=Q(comment_comments__parent__isnull=False), distinct=True),
-				desktops=Count('desktop', distinct=True),
-				topics=Count('topic', distinct=True),
-				news_count=Count('news', filter=Q(news__approved=True), distinct=True),
-				tweets=Count('tweet', distinct=True),
-				wkiki_pages=Count('page', distinct=True),
-			)
-			.values_list('username', 'pk', 'articles', 'blogs', 'comments', 'desktops', 'topics', 'news_count', 'tweets', 'wkiki_pages'))
-
-		for user in users:
+		writer.write_row(['username', 'pk'] + fields)
+		for user in logged_users_stats:
 			writer.write_row(user)
-
 		writer.close()
