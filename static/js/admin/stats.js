8368626271c959ccf0bd984f28632cad1135128b
Miroslav Bendík <miroslav.bendik@gmail.com>
2020-05-06 21:21:08 +0200

    Odstránený jquery z kódu štatistík

--- static/js/admin/stats.js
+++ static/js/admin/stats.js
@@ -1,78 +1,115 @@
-jQuery(function($) {
-	var statsContainer = $('.stats');
-	var statsLink = $('a', statsContainer).last();
-	var statsContent = $('.dashboard-module-content', statsContainer);
+(function() {
+document.addEventListener("DOMContentLoaded", function() {
 
-	var loadTabContent = function(tab) {
-		var container = tab.parentNode;
-		var module_id = tab.id.replace(/_stats_chart$/, "");
-		var selectVal = $('select', container.parentNode).val();
-		if (!selectVal) {
-			return;
-		}
+var statsContainer = document.querySelector('.stats');
+if (statsContainer === null) {
+	return;
+}
+var statsLink = statsContainer.getElementsByTagName('a')[0];
+if (statsLink === undefined) {
+	return;
+}
+var statsContent = statsContainer.querySelector('.dashboard-module-content');
+if (statsContent === null) {
+	return;
+}
 
-		var loadGraphFunction = function(tabNode, url) {
-			return function() {
-				return new Dygraph(tabNode, url + "&format=csv");
-			}
-		}(tab, selectVal);
-		setTimeout(loadGraphFunction, 100);
+
+function loadTabContent(tab) {
+	var container = tab.parentNode.parentNode;
+	var module_id = tab.id.replace(/_stats_chart$/, "");
+	var select = container.getElementsByTagName('select')[0];
+	var selectVal = select.value;
+	if (!selectVal) {
+		return;
 	}
 
-	var processStats = function(data) {
-		var groupTabs = $('<div class="group group-tabs"></div>');
-		statsContent.append(groupTabs);
-		//var tabs = $('<ul></ul>');
-		var tabs = $('<ul id="suit_form_tabs" class="nav nav-tabs nav-tabs-suit" data-tab-prefix="suit-tab"></ul>');
-		groupTabs.append(tabs);
+	var loadGraphFunction = function(tabNode, url) {
+		return function() {
+			return new Dygraph(tabNode, url + "&format=csv");
+		};
+	}(tab, selectVal);
+	setTimeout(loadGraphFunction, 100);
+}
 
-		for (var j = 0; j < data.length; ++j) {
-			var id = data[j][0];
-			var info = data[j][1];
-			var choices = info.choices;
-			var tab = $('<li class="group-tabs-link"></li>');
-			var tabLink = $('<a href="#module_' + id + '"></a>');
-			tabLink.text(info.name);
-			tab.append(tabLink);
-			tabs.append(tab);
 
-			//var submodule = $('<div id="module_' + id + '" class="dashboard-module"></div>');
-			var submodule = $('<div id="module_' + id + '" class="dashboard-module suit-tab suit-tab-module_' + id + ' well" style="margin-top: -15px;"></div>');
-			groupTabs.append(submodule);
+function processStats(data) {
+	var groupTabs = document.createElement('div');
+	groupTabs.className = 'group group-tabs stats';
+	statsContent.appendChild(groupTabs);
+	var tabs = document.createElement('ul');
+	tabs.setAttribute('id', 'suit_form_tabs');
+	tabs.className = 'nav nav-tabs nav-tabs-suit';
+	tabs.setAttribute('data-tab-prefix', 'suit-tab');
+	groupTabs.appendChild(tabs);
 
-			var sel = $('<select></select>');
-			for (var i = 0; i < choices.length; ++i) {
-				var choice = choices[i];
-				var opt = $('<option></option>');
-				opt.attr('value', choice.url);
-				opt.text(choice.label);
-				sel.append(opt);
-			}
-			submodule.append(sel);
+	data.forEach(function(group) {
+		var id = group[0];
+		var info = group[1];
+		var choices = info.choices;
+		var tab = document.createElement('li');
+		tab.className = 'group-tabs-link';
+		var tabLink = document.createElement('a');
+		tabLink.setAttribute('href', '#module_' + id);
+		tabLink.appendChild(document.createTextNode(info.name));
+		tab.appendChild(tabLink);
+		tabs.appendChild(tab);
 
-			var submoduleContent = $('<div class="dashboard-module-content"></div>');
-			submodule.append(submoduleContent);
-			var chart = $('<div id="' + id + '" class="stats_chart" style="height: 200px;"></div>');
-			submoduleContent.append(chart);
+		var submodule = document.createElement('div');
+		submodule.setAttribute('id', 'module_' + id);
+		submodule.className = 'dashboard-module suit-tab suit-tab-module_' + id + ' well';
+		groupTabs.appendChild(submodule);
 
-			sel.bind('change', (function(chart) { return function() { loadTabContent(chart); };} )(chart[0]));
+		var sel = document.createElement('select');
+		for (var i = 0; i < choices.length; ++i) {
+			var choice = choices[i];
+			var opt = document.createElement('option');
+			opt.setAttribute('value', choice.url);
+			opt.appendChild(document.createTextNode(choice.label));
+			sel.append(opt);
 		}
+		submodule.appendChild(sel);
 
-		//$('.stats .group-tabs').tabs();
-		$('#suit_form_tabs').find("a").click(function(e) {
-			loadTabContent($('.suit-tab' + $(this).attr('href').replace("#", "-") + ' .stats_chart')[0]);
+		var submoduleContent = document.createElement('div');
+		submoduleContent.className = 'dashboard-module-content';
+		submodule.appendChild(submoduleContent);
+		var chart = document.createElement('div');
+		chart.setAttribute('id', id + '_stats_chart');
+		chart.className = 'stats-chart';
+		submoduleContent.appendChild(chart);
+
+		sel.onchange = (function(chart) { return function() { loadTabContent(chart); };} )(chart);
+	});
+
+	tabs.onclick = function(e) {
+		var target = e.target;
+		if (target.tagName.toLowerCase() === 'a') {
+			var id = target.getAttribute('href').slice(8);
+			var tab = document.getElementById(id + '_stats_chart');
+			loadTabContent(tab);
 			e.preventDefault();
-		});
-		$('#suit_form_tabs').suit_form_tabs();
-		//$('.stats .group-tabs').bind('tabsactivate', function(event, ui) { loadTabContent($('.stats_chart', ui.newPanel)[0]); });
-		//loadTabContent($('.stats_chart')[0]);
+		}
 	};
 
-	var loadStats = function() {
-		var statsHref = statsLink.attr('href');
-		statsLink.remove();
-		$.getJSON(statsHref, processStats);
-	};
+	loadTabContent(groupTabs.querySelector('.stats-chart'));
+}
 
-	loadStats();
+
+function loadStats() {
+	var statsHref = statsLink.getAttribute('href');
+	statsLink.remove();
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			var json = JSON.parse(xhr.responseText);
+			processStats(json);
+		}
+	};
+	xhr.open("GET", statsHref, true);
+	xhr.send(null);
+}
+
+
+loadStats();
 });
+}());
