8982c7e321d3db877df9452eba7ec17006b804df
lucorp <stefan.lucivjansky@gmail.com>
2020-05-23 18:33:46 +0200

    Stranka ochrany osobnych udajov aj s linkom v paticke [fixes #125]

--- linuxos/static_urls.py
+++ linuxos/static_urls.py
@@ -24,6 +24,8 @@ sites = (
 	('officescreens', 'kancelaria/screenshoty'),
 	('mmscreens', 'multimedia/screenshoty'),
 	('vedascreens', 'veda/screenshoty'),
+
+        ('ochrana_osobnych_udajov', 'ochrana-osobnych-udajov'),
 )
 
 sites_urls = [url('^' + u[1] + '/$', TemplateView.as_view(template_name='static/' + u[1] + '.html'), name="page_" + u[1]) for u in sites]
