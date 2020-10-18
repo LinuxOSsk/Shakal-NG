d5cfcb60f7cf6484aa3c6b8dad973f3e3e5b9a7d
Miroslav Bendík <miroslav.bendik@gmail.com>
2020-10-18 16:56:55 +0200

    Oprava skriptu

--- static/js/user-posts.js
+++ static/js/user-posts.js
@@ -3,7 +3,9 @@
 var HSVtoRGB = function(h, s, v) {
 	var r, g, b, i, f, p, q, t;
 	if (arguments.length === 1) {
-		s = h.s, v = h.v, h = h.h;
+		s = h.s;
+		v = h.v;
+		h = h.h;
 	}
 	i = Math.floor(h * 6);
 	f = h * 6 - i;
@@ -11,12 +13,12 @@ var HSVtoRGB = function(h, s, v) {
 	q = v * (1 - f * s);
 	t = v * (1 - (1 - f) * s);
 	switch (i % 6) {
-		case 0: r = v, g = t, b = p; break;
-		case 1: r = q, g = v, b = p; break;
-		case 2: r = p, g = v, b = t; break;
-		case 3: r = p, g = q, b = v; break;
-		case 4: r = t, g = p, b = v; break;
-		case 5: r = v, g = p, b = q; break;
+		case 0: r = v; g = t; b = p; break;
+		case 1: r = q; g = v; b = p; break;
+		case 2: r = p; g = v; b = t; break;
+		case 3: r = p; g = q; b = v; break;
+		case 4: r = t; g = p; b = v; break;
+		case 5: r = v; g = p; b = q; break;
 	}
 	return {
 		r: Math.round(r * 255),
@@ -27,7 +29,9 @@ var HSVtoRGB = function(h, s, v) {
 
 var RGBtoHSV = function(r, g, b) {
 	if (arguments.length === 1) {
-		g = r.g, b = r.b, r = r.r;
+		g = r.g;
+		b = r.b;
+		r = r.r;
 	}
 	var max = Math.max(r, g, b), min = Math.min(r, g, b),
 		d = max - min,
@@ -54,14 +58,15 @@ var BarChartPlotter = function(e) {
 	var points = e.points;
 	var y_bottom = e.dygraph.toDomYCoord(0);
 
+	var i;
 	var bar_width = 1;
 	var xmin = points[0].xval;
 	var xmax = points[points.length - 1].xval;
 	var xdiff = xmax - xmin;
-	for (var i = 0; i < points.length - 1; i++) {
+	for (i = 0; i < points.length - 1; i++) {
 		xdiff = Math.min(points[i+1].xval - points[i].xval, xdiff);
 	}
-	var bar_width = 2/3 * (points[1].canvasx - points[0].canvasx) * (xdiff / (points[1].xval - points[0].xval));
+	bar_width = 2/3 * (points[1].canvasx - points[0].canvasx) * (xdiff / (points[1].xval - points[0].xval));
 
 	var color = new RGBColorParser(e.color);
 	color.r = Math.floor((255 + color.r) / 2);
@@ -69,7 +74,7 @@ var BarChartPlotter = function(e) {
 	color.b = Math.floor((255 + color.b) / 2);
 	ctx.fillStyle = color.toRGB();
 
-	for (var i = 0; i < points.length; i++) {
+	for (i = 0; i < points.length; i++) {
 		var p = points[i];
 		var center_x = p.canvasx;
 
@@ -82,16 +87,17 @@ var calcColor = function(val) {
 	var left = [0.1, 0.1, 0.98];
 	var mid = [0.2975, 1.00, 0.965];
 	var right = [0.2975, 1.0, 0.2];
+	var pos, rpos, rgb;
 
 	if (val < 0.5) {
-		var pos = val * 2;
-		var rpos = 1.0 - pos;
-		var rgb = HSVtoRGB(mid[0] * pos + left[0] * rpos, mid[1] * pos + left[1] * rpos, mid[2] * pos + left[2] * rpos);
+		pos = val * 2;
+		rpos = 1.0 - pos;
+		rgb = HSVtoRGB(mid[0] * pos + left[0] * rpos, mid[1] * pos + left[1] * rpos, mid[2] * pos + left[2] * rpos);
 	}
 	else {
-		var pos = (val - 0.5) * 2;
-		var rpos = 1.0 - pos;
-		var rgb = HSVtoRGB(right[0] * pos + mid[0] * rpos, right[1] * pos + mid[1] * rpos, right[2] * pos + mid[2] * rpos);
+		pos = (val - 0.5) * 2;
+		rpos = 1.0 - pos;
+		rgb = HSVtoRGB(right[0] * pos + mid[0] * rpos, right[1] * pos + mid[1] * rpos, right[2] * pos + mid[2] * rpos);
 	}
 
 	return 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')';
@@ -100,7 +106,7 @@ var calcColor = function(val) {
 var countWeeks = 0;
 var maxDayValue = 0;
 
-_.forEach(daily_stats, function(day, i) {
+daily_stats.forEach(function(day, i) {
 	if (day[0][3] === 0 || i === 0) {
 		countWeeks++;
 	}
@@ -109,7 +115,7 @@ _.forEach(daily_stats, function(day, i) {
 
 var weekPercentWidth = 100 / (countWeeks + 1);
 var dailyStatsContainer = _.id('daily_stats');
-var currentColumn = undefined;
+var currentColumn;
 var transparentImg = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
 
 var columnOuter = document.createElement('DIV');
@@ -147,7 +153,7 @@ for (var i = 0; i < 8; ++i) {
 	columnOuter.appendChild(createTextBlock(days[i]));
 }
 
-var currentMonth = undefined;
+var currentMonth;
 var currentDay = current_day;
 if (currentDay !== undefined) {
 	currentDay = current_day.split('-');
@@ -156,7 +162,7 @@ if (currentDay !== undefined) {
 	currentDay[2] = parseInt(currentDay[2], 10);
 }
 
-_.forEach(daily_stats, function(day, i) {
+daily_stats.forEach(function(day, i) {
 	if (currentColumn === undefined || day[0][3] === 0) {
 		var isFirst = currentColumn === undefined;
 		var columnOuter = document.createElement('DIV');
@@ -250,7 +256,7 @@ if (monthly_stats.length < 2) {
 
 var skip = true;
 var data = [];
-_.forEach(monthly_stats, function(day, i) {
+monthly_stats.forEach(function(day, i) {
 	if (day[1] == 0 && skip) {
 		return;
 	}
@@ -271,4 +277,4 @@ var opts = {
 };
 var graph = new Dygraph(monthlyStatsContainer, data, opts);
 
-}(_utils));
+}(_utils2));
