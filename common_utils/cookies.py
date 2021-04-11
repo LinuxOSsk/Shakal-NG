2e839ac1e28f34a6aff27ade5ab99ac153d2cde4
Miroslav Bendík <miroslav.bendik@gmail.com>
2021-04-11 06:46:25 +0200

    Povolené extra parametre pre set_cookie

--- common_utils/cookies.py
+++ common_utils/cookies.py
@@ -9,18 +9,23 @@ from django.conf import settings
 DEFAULT_COOKIE_AGE = 3600 * 24 * 365 # rok
 
 
-def set_cookie(response, cookie_name, cookie_value, cookie_age=None):
+def set_cookie(response, cookie_name, cookie_value, cookie_age=None, **kwargs):
 	if cookie_age is None:
 		cookie_age = DEFAULT_COOKIE_AGE
 	expires = datetime.utcnow() + timedelta(seconds=cookie_age)
+
+	extra = {
+		'domain': settings.SESSION_COOKIE_DOMAIN,
+		'path': settings.SESSION_COOKIE_PATH,
+		'secure': settings.SESSION_COOKIE_SECURE or None,
+		'httponly': settings.SESSION_COOKIE_HTTPONLY or None,
+	}
+	extra.update(kwargs)
 
 	response.set_cookie(
 		cookie_name,
 		cookie_value,
 		max_age=None,
 		expires=expires,
-		domain=settings.SESSION_COOKIE_DOMAIN,
-		path=settings.SESSION_COOKIE_PATH,
-		secure=settings.SESSION_COOKIE_SECURE or None,
-		httponly=settings.SESSION_COOKIE_HTTPONLY or None
+		**extra
 	)
