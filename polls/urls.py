f11654b41b023118c2a0b86cfdf134bfca36b4ec
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-05-13 11:25:25 +0200

    fix: Force CSRF token for polls

--- polls/urls.py
+++ polls/urls.py
@@ -10,6 +10,6 @@ app_name = 'polls'
 urlpatterns = [
 	path('detail/<slug:slug>/', views.PollDetail.as_view(), name='detail-by-slug'),
 	path('<page:page>', views.PollList.as_view(), name='list'),
-	path('post/<int:pk>/', csrf_exempt(views.PollPost.as_view()), name='post'),
+	path('post/<int:pk>/', views.PollPost.as_view(), name='post'),
 	path('vytvorit/', views.PollCreate.as_view(), name='create'),
 ]
