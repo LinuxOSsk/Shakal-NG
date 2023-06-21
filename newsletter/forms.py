d20e2a86011e7fcace83718b90eb347805d856a1
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-06-21 06:33:21 +0200

    feat: Preparing unsign action

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from django.core.validators import ValidationError
 
+from .api import unsign_email
 from .models import NewsletterSubscription
 
 
@@ -10,4 +12,14 @@ class NewsletterSubscribeForm(forms.Form):
 
 class NewsletterUnsubscribeForm(forms.Form):
 	email = forms.HiddenInput()
-	token = forms.HiddenInput()
+
+	def clean(self):
+		cleaned_data = super().clean()
+		email = unsign_email(cleaned_data.get('email', ''))
+		if email is None:
+			raise ValidationError("Neplatný odkaz")
+		cleaned_data['email'] = email
+		return cleaned_data
+
+
+# TODO: unsubscribe without link using e-mail form
