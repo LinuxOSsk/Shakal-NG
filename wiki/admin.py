6d77e7f6c2180612cedf6949dc01af94f06783b3
Miroslav Bendík <miroslav.bendik@gmail.com>
2024-04-06 18:49:49 +0200

    refactor: Replaced MPTTModelAdmin in wiki page section

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from mptt.admin import MPTTModelAdmin
 from reversion.admin import VersionAdmin
 
 from .models import Page
+from common_utils.admin import render_tree_depth
 
 
-class PageAdmin(VersionAdmin, MPTTModelAdmin):
-	list_display = ('title', 'slug', )
+class PageAdmin(VersionAdmin, admin.ModelAdmin):
+	list_display = ('get_title', 'slug', )
 	search_fields = ('title', 'original_text', )
 	ordering = ('-id', )
 	prepopulated_fields = {'slug': ('title', )}
 	raw_id_fields = ('parent', 'last_author',)
+
+	def get_queryset(self, request):
+		return super().get_queryset(request).with_tree_fields()
+
+	def get_title(self, obj):
+		return f'{render_tree_depth(obj)} {obj.title}'
+	get_title.short_description = Page._meta.get_field('title').verbose_name
+	get_title.admin_order_field = 'title'
+
 
 admin.site.register(Page, PageAdmin)
